546b3312a633e3cc892d2db85bb4e2be56c6ad82
Luis Mayta <slovacus@gmail.com>
2015-01-17 23:22:52 -0500

    added packages ruby

--- custom.el
+++ custom.el
@@ -1,26 +1,3 @@
-;; Don't move back the cursor one position when exiting insert mode' 
-(setq evil-move-cursor-back nil)
-
-;; evil leader
-(global-evil-leader-mode)
-
-;;set evil leader
-(evil-leader/set-leader ",")
-
-;; mapping keys evil leader
-(evil-leader/set-key
-  "e" 'find-file
-  "n" 'neotree-toggle
-  "f" 'fiplr-find-file
-  "b" 'switch-to-buffer
-  "k" 'kill-buffer)
-
-;; show whitespace...
-(global-whitespace-mode t)
-
-;; reload changes from disk
-(global-auto-revert-mode t)
-
 ;; complete parenthesis
 ;(setq skeleton-pair t)
 ;(global-set-key "[" 'skeleton-pair-insert-maybe)
