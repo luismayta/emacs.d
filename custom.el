45588ccbcdbd0a49a249687a127997963153f4b9
@slovacus <slovacus@gmail.com>
2015-04-03 07:06:13 -0500

    style: separated config dependences (#7)

--- custom.el
+++ custom.el
@@ -5,15 +5,3 @@
 ;(global-set-key "{" 'skeleton-pair-insert-maybe)
 ;(global-set-key "'" 'skeleton-pair-insert-maybe)
 ;(global-set-key "\"" 'skeleton-pair-insert-maybe)
-
-;; make jedi-mode
-(autoload 'jedi:setup "jedi" nil t)
-(setq jedi:setup-keys t)
-
-;;python mode
-(add-hook 'python-mode-hook
-  (lambda ()
-    (jedi:setup)
-    ;(elpy-enable)
-    ;(elpy-use-ipython)
-    (setq python-python-command "ipython")))
