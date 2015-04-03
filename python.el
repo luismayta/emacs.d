6927a5e6796906369bbddb91491e4638bea5d28f
Luis Mayta <slovacus@gmail.com>
2015-04-03 06:52:37 -0500

    [feature/separation] added file conf python

---
+++ python.el
@@ -0,0 +1,15 @@
+;; python configuration
+
+(require 'python-mode)
+
+;; make jedi-mode
+(autoload 'jedi:setup "jedi" nil t)
+(setq jedi:setup-keys t)
+
+;;python mode
+(add-hook 'python-mode-hook
+  (lambda ()
+    (jedi:setup)
+    ;(elpy-enable)
+    ;(elpy-use-ipython)
+    (setq python-python-command "ipython")))
