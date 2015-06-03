64770ea2f2486f77e3b36ef75569c24b312e92c9
@slovacus <slovacus@gmail.com>
2015-06-03 08:25:23 -0500

    chore: implement package go code

--- config/go.el
+++
@@ -1,9 +0,0 @@
-;;requires modules
-(require 'go-eldoc) ;; Don't need to require, if you install by package.el
-(require 'go-mode) ;; Don't need to require, if you install by package.el
-
-(add-hook 'go-mode-hook 'go-eldoc-setup)
-
-(set-face-attribute 'eldoc-highlight-function-argument nil
-                    :underline t :foreground "green"
-                                        :weight 'bold)
