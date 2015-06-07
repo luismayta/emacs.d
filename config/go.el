ec631a45ec6f0b62946b8f65cc8c0ce046db2f7d
@slovacus <slovacus@gmail.com>
2015-06-06 19:09:41 -0500

    [feature/go] added packages go

--- config/go.el
+++ config/go.el
@@ -10,7 +10,8 @@
 (require 'go-eldoc)
 (require 'go-mode)
 (require 'golint)
-;;(require 'go-flymake)
+(require 'go-flymake)
+(require 'go-autocomplete)
 ;;(require 'company-mode)
 
 (add-hook 'go-mode-hook 'go-eldoc-setup)
@@ -24,7 +25,7 @@
 (load "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
 (add-hook 'go-mode-hook 'go-oracle-mode)
 
-(add-to-list 'load-path "$GOPATH/src/github.com/dougm/goflymake")
+;;(add-to-list 'load-path "$GOPATH/src/github.com/dougm/goflymake")
 
 ;;(add-hook 'go-mode-hook 'company-mode)
 ;;(add-hook 'go-mode-hook (lambda ()
