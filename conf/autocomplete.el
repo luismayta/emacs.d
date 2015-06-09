f5300513dfac176231be8ab8630ff44b67d9118c
@slovacus <slovacus@gmail.com>
2015-06-09 06:08:15 -0500

    [feature/el-get] fixed code auto-complete

--- conf/autocomplete.el
+++ conf/autocomplete.el
@@ -1,3 +1,7 @@
 ;; Install Packages
 (bundle 'auto-complete nil)
 (require 'auto-complete)
+
+;; turn on auto-completion of function names etc
+(require 'auto-complete)
+(global-auto-complete-mode t)
