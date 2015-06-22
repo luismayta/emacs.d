88cfcce7af2cbcfd9377544669aa7bd412c1f1b2
@slovacus <slovacus@gmail.com>
2015-06-22 01:43:35 -0500

    refactor: configuration functions magit (#6)

--- conf/git.el
+++ conf/git.el
@@ -4,3 +4,9 @@
 (bundle 'git-rebase-mode nil)
 (bundle 'gitconfig-mode nil)
 (bundle 'gitignore-mode nil)
+
+;; mapping keys evil leader
+(evil-leader/set-key
+  "gs" 'magit-status
+  "gc" 'magit-commit
+  "gd" 'magit-diff)
