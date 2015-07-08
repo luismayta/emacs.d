e833c20a1bcbf7c99bcc10d45ec11a78cd5d499c
@slovacus <slovacus@gmail.com>
2015-07-08 02:48:12 -0500

    [feature/#4] clean package not exist melpa

--- conf/git.el
+++ conf/git.el
@@ -1,9 +1,9 @@
 ;; Install Packages
 (bundle 'magit nil)
-(bundle 'git-commit-mode nil)
-(bundle 'git-rebase-mode nil)
 (bundle 'gitconfig-mode nil)
 (bundle 'gitignore-mode nil)
+
+(require 'magit)
 
 ;; mapping keys evil leader
 (evil-leader/set-key
