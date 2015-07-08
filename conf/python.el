d858118db8ded1bf7d5837b8cebb8c992728e281
@slovacus <slovacus@gmail.com>
2015-07-08 02:54:20 -0500

    fix: install el-get (#4)

--- conf/python.el
+++ conf/python.el
@@ -1,17 +1,16 @@
 ;; python configuration
 (bundle 'python-mode nil)
 
-(bundle 'python-mode nil)
-
 (require 'python-mode)
 
-;; (bundle 'jedi nil)
-;; (require 'jedi)
+(bundle 'jedi nil)
+(require 'jedi)
 
-;; (add-hook 'python-mode-hook
-;;   (lambda ()
-;;     (jedi:setup)
-;;     (setq jedi:setup-keys t)
-;;     ;(elpy-enable)
-;;     ;(elpy-use-ipython)
-;;     (setq python-python-command "ipython")))
+;;python mode
+(add-hook 'python-mode-hook
+  (lambda ()
+    (jedi:setup)
+    (setq jedi:setup-keys t)
+    ;(elpy-enable)
+    ;(elpy-use-ipython)
+    (setq python-python-command "ipython")))
