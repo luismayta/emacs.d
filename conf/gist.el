2399720c81d8700ad1a6002720e106f4fc587e2e
@slovacus <slovacus@gmail.com>
2015-07-10 17:41:51 -0500

    build: clean directory unnecessary (#11)

---
+++ conf/gist.el
@@ -0,0 +1,11 @@
+;; Install Packages
+(bundle 'git nil)
+(bundle 'gist nil)
+
+(require 'git)
+(require 'gist)
+
+;; mapping keys evil leader
+(evil-leader/set-key
+  "gl" 'gist-list
+  "gb" 'gist-buffer)
