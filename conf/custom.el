8c03e42052ff48bfc13e2c466f0aaa90d535be41
@slovacus <slovacus@gmail.com>
2015-07-10 22:11:05 -0500

    [feature/#13] Order Functions, Implement Hooks
    separated in files:
    functions
    hooks

--- conf/custom.el
+++ conf/custom.el
@@ -87,18 +87,3 @@
 
 (set-frame-font "DejaVuSansMono:pixelsize=12")
 (set-default-font "DejaVuSansMono 12")
-
-(defun bury-compile-buffer-if-successful (buffer string)
-  "Bury a compilation buffer if succeeded without warnings"
-  (if (and
-        (string-match "compilation" (buffer-name buffer))
-        (string-match "finished" string)
-        (not
-          (with-current-buffer buffer
-            (search-forward "warning" nil t))))
-    (run-with-timer 1 nil
-      (lambda (buf)
-        (bury-buffer buf)
-        (switch-to-prev-buffer (get-buffer-window buf) 'kill))
-      buffer)))
-(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
