dcf8f4d49d2d2a0a0b78b5d36a55467fb43b66cc
@slovacus <slovacus@gmail.com>
2015-08-02 12:48:38 -0500

    chore: implement added config helm-projectile (#27)

--- conf/projectile.el
+++ conf/projectile.el
@@ -1,10 +1,14 @@
 ;; Install Packages
 (bundle 'projectile nil)
+(bundle 'helm-projectile nil)
+
 (require 'projectile)
 
 (projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
 (setq projectile-file-exists-local-cache-expire (* 5 60))
 
 ;; mapping keys evil leader
 (evil-leader/set-key
-  "fp" 'projectile-find-file)
+  "fp" 'helm-projectile-find-file)
