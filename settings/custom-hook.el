15f33f5fb81a60472094d4780e8501d6a0c5c048
@slovacus <slovacus@gmail.com>
2015-08-26 06:12:57 -0500

    chore: implement generate file custom hooks (#29)

---
+++ settings/custom-hook.el
@@ -0,0 +1,11 @@
+;----------------------;
+;;; Custom Hooks     ;;;
+;----------------------;
+
+(require 'whitespace)
+
+(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
+
+(add-hook 'before-save-hook 'hook-save-buffer)
+
+(provide 'custom-hooks)
