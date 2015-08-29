e18ce82e1054b69918ddef2fffe1268c7907eb86
@slovacus <slovacus@gmail.com>
2015-08-29 18:46:41 -0500

    [feature/#32] Added package helm

---
+++ settings/helm-settings.el
@@ -0,0 +1,22 @@
+;--------------------------------;
+;;;    Helm Settings           ;;;
+;--------------------------------;
+
+;; Install Packages
+(bundle 'helm nil)
+(bundle 'helm-descbinds nil)
+
+(require 'helm)
+(require 'helm-descbinds)
+
+(fset 'describe-bindings 'helm-descbinds)
+(helm-mode t)
+
+(global-set-key (kbd "C-c h") 'helm-mini)
+
+;; mapping keys evil leader
+(evil-leader/set-key
+  "h" 'helm-mini
+  )
+
+(provide 'helm-settings)
