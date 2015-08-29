2f375631e2cd07ddc37129ddfdb423aeb14c3931
@slovacus <slovacus@gmail.com>
2015-08-29 18:25:40 -0500

    [feature/#31] Added functions copy and paste

--- settings/custom-functions.el
+++ settings/custom-functions.el
@@ -45,4 +45,18 @@
   (when refresh (package-refresh-contents))
   (package-install depname))
 
+;; functions copy and paste system
+
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx) 
+
 (provide 'custom-functions)
