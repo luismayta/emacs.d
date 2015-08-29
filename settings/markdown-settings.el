aa1cd306d792f49e623ce3c33df6f879dbae5f68
@slovacus <slovacus@gmail.com>
2015-08-29 18:22:03 -0500

    chore: implement other files to markdown (#30)

--- settings/markdown-settings.el
+++ settings/markdown-settings.el
@@ -1,7 +1,17 @@
+;-------------------;
+;;; Markdown mode ;;;
+;-------------------;
+
 ;; Install Packages
 (bundle 'markdown-mode nil)
 ;; Config
 (require 'markdown-mode)
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(defun markdown-mode-hook ()
+  (autoload 'markdown-mode "markdown-mode" nil t)
+  (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+  )
 
 (provide 'markdown-settings)
