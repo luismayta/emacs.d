92c0dfa16692f84470f31122f1023ec73e2c119a
@slovacus <slovacus@gmail.com>
2015-08-29 19:18:16 -0500

    [feature/#31] Added package clipboard

--- init.el
+++ init.el
@@ -91,6 +91,7 @@
 ;; configure general settings
 (require 'default-settings)
 (require 'editorconfig-settings)
+(require 'clipboard-settings)
 (require 'evil-settings)
 (require 'fixmee-settings)
 (require 'projectile-settings)
