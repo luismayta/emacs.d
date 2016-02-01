81f4ed0c285c8b141655c983a6290256a7331045
@slovacus <slovacus@gmail.com>
2016-02-01 05:52:52 -0500

    fix: components go (#41)

--- settings/go-settings.el
+++ settings/go-settings.el
@@ -5,52 +5,40 @@
 ;;go get -u github.com/dougm/goflymake
 ;;install package company-go
 ;;http://yousefourabi.com/blog/2014/05/emacs-for-go/
-;;http://dominik.honnef.co/posts/2013/03/writing_go_in_emacs/
 
 (bundle 'go-mode nil)
 (bundle 'go-eldoc nil)
 (bundle 'golint nil)
-;;(bundle 'go-flymake nil)
 (bundle 'go-autocomplete nil)
 
+(require 'auto-complete-config)
 (require 'go-eldoc)
 (require 'go-mode)
 (require 'golint)
-;;(require 'go-flymake)
 (require 'go-autocomplete)
-;;(require 'company-mode)
+(ac-config-default)
 
-(add-hook 'go-mode-hook 'go-eldoc-setup)
+(defun go-mode-setup ()
+  (setq compile-command "go build -v && go test -v && go vet")
+  (define-key (current-local-map) "\C-c\C-c" 'compile)
+  (go-eldoc-setup)
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'before-save-hook 'go-remove-unused-imports)
+  (local-set-key (kbd "M-.") 'godef-jump))
+
+(add-hook 'go-mode-hook 'go-mode-setup)
+
 
 (set-face-attribute 'eldoc-highlight-function-argument nil
                     :underline t :foreground "green"
                                         :weight 'bold)
 
-(add-hook 'before-save-hook 'gofmt-before-save)
+;; (add-hook 'go-mode-hook '(lambda ()
+;;                              (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
 
-;; (load "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
-;; (add-hook 'go-mode-hook 'go-oracle-mode)
 
-;;(add-to-list 'load-path "$GOPATH/src/github.com/dougm/goflymake")
-
-;;(add-hook 'go-mode-hook 'company-mode)
-;;(add-hook 'go-mode-hook (lambda ()
-;;                          (set (make-local-variable 'company-backends) '(company-go))
-;;                            (company-mode)))
-
-;;shortcuts
-
-(add-hook 'go-mode-hook '(lambda ()
-                             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-
-(add-hook 'go-mode-hook '(lambda ()
-                             (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
-
-(add-hook 'go-mode-hook '(lambda ()
-                           (local-set-key (kbd "C-c C-f") 'gofmt)))
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-(add-hook 'go-mode-hook '(lambda ()
-                             (local-set-key (kbd "C-c C-k") 'godoc)))
+;; (add-hook 'go-mode-hook '(lambda ()
+;;                              (local-set-key (kbd "C-c C-k") 'godoc)))
 
 (provide 'go-settings)
