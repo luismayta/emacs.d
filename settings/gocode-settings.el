62cc474a0d069081c3ec7ae0ac16cb2fd2ff7bd0
@slovacus <slovacus@gmail.com>
2016-04-18 00:45:54 -0500

    [#41] Added gocode settings

---
+++ settings/gocode-settings.el
@@ -0,0 +1,36 @@
+;; -*- Emacs-Lisp -*-
+;; Settings for `gocode'.
+
+;; Copyright (C) 2014 Dylan.Wen
+
+;; Author: Dylan.Wen <hhkbp2@gmail.com>
+;; Time-stamp: <2014-04-10 14:48>
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(require 'auto-complete)
+(require 'go-autocomplete)
+
+
+(defun gocode-settings ()
+  "Settings for `gocode'."
+
+  )
+
+(provide 'gocode-settings)
