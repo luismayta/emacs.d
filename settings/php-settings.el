7f97600dfd7998de6f946e4e14a1f593dedb4de3
@slovacus <slovacus@gmail.com>
2016-07-10 09:32:28 -0500

    [#54] Added assign key evil leader

--- settings/php-settings.el
+++ settings/php-settings.el
@@ -9,9 +9,6 @@
 ;; make php-mode indentation vaguely sane
 (add-hook 'php-mode-hook
   (lambda ()
-    (setq indent-tabs-mode t)
-    (setq tab-width 4)
-    (setq evil-shift-width 4)
     (set (make-local-variable 'tab-stop-list) (number-sequence 4 200 4))
     (c-set-offset 'substatement-open 0)))
 
