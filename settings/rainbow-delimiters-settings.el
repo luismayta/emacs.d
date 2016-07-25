81546115ad394e820bd9db6b03f6b09203466695
@slovacus <slovacus@gmail.com>
2016-07-25 04:01:23 -0500

    [#56] Added settings rainbow-delimiters

---
+++ settings/rainbow-delimiters-settings.el
@@ -0,0 +1,12 @@
+;-----------------------------------------;
+;;;    Rainbow-Delimiters Settings      ;;;
+;-----------------------------------------;
+
+;; Install Packages
+(bundle 'rainbow-delimiters nil)
+
+(require 'rainbow-delimiters)
+
+(global-rainbow-delimiters-mode)
+
+(provide 'rainbow-delimiters-settings)
