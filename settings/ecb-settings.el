d326680607d344cce9c3591801495d821ce7dace
@slovacus <slovacus@gmail.com>
2016-07-27 22:23:05 -0500

    [#61] Fixed load ecb

--- settings/ecb-settings.el
+++ settings/ecb-settings.el
@@ -3,14 +3,8 @@
 ;--------------------------;
 
 ;; Install Packages
-(el-get-install 'ecb nil)
+(bundle 'ecb nil)
 
 (require 'ecb)
-
-(setq ecb-auto-activate t)
-(setq ecb-layout-name "left13")
-(setq ecb-new-ecb-frame nil)
-(setq ecb-tip-of-the-day nil)
-(setq ecb-windows-width 13)
 
 (provide 'ecb-settings)
