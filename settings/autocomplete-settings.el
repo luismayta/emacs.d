025d9abd4025986050080746bcf89d92df38f59f
@slovacus <slovacus@gmail.com>
2016-07-28 12:08:58 -0500

    fix: settings configuration (#64)

--- settings/autocomplete-settings.el
+++ settings/autocomplete-settings.el
@@ -3,8 +3,9 @@
 (require 'auto-complete)
 
 ;; turn on auto-completion of function names etc
-(ac-auto-start nil)
-(ac-trigger-key "TAB")
+(setq ac-auto-start nil)
+(setq ac-trigger-key "TAB")
+
 (global-auto-complete-mode t)
 (auto-complete-mode t)
 
