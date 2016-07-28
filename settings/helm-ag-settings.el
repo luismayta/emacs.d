a746cf53bb6187df4e6bdf1ce1618bac1ba76a8f
@slovacus <slovacus@gmail.com>
2016-07-28 11:12:15 -0500

    [#46] Added helm-ag settings

---
+++ settings/helm-ag-settings.el
@@ -0,0 +1,10 @@
+;-----------------------------------;
+;;;    Helm ag Settings           ;;;
+;-----------------------------------;
+
+;; Install Packages
+(bundle 'helm-ag nil)
+
+(require 'helm-ag)
+
+(provide 'helm-ag-settings)
