3664714b62621c1c6abfebe7d0bbc3e8601be5e8
@slovacus <slovacus@gmail.com>
2016-08-21 18:01:26 -0500

    [#69] Fix: activate yasnippet settings

--- settings/yasnippet-settings.el
+++ settings/yasnippet-settings.el
@@ -1,52 +1,22 @@
 ;; Install packages
 
 (bundle 'yasnippet nil)
-(bundle 'yasnippet-bundle nil)
+;; (bundle 'yasnippet-bundle nil)
 
 (require 'yasnippet)
-(require 'yasnippet-bundle)
+;; (require 'yasnippet-bundle)
 
-(defun do-yas-expand ()
-  (let ((yas-fallback-behavior 'return-nil))
-    (yas-expand)))
+(setq yas-snippet-dirs '(
+        "~/.emacs.d/snippets" ;personal snippets
+        ))
+(setq
+ yas-prompt-functions '(yas/ido-prompt yas/completing-prompt yas/x-prompt yas/dropdown-prompt yas/no-prompt)
+ yas-indent-line 'auto)
 
-(defun force-yasnippet-off ()
-  (setq-local yas-dont-activate t)
-  (yas-minor-mode -1))
+(yas/global-mode t)
+(yas/reload-all)
 
-(defun mb/handle-tab ()
-  (interactive)
-  (cond
-    ((minibufferp)
-    (minibuffer-complete))
-    ((string= mode-name "Org")
-    (when (null (do-yas-expand))
-      (org-cycle)))
-    ((string= mode-name "Magit")
-    (magit-section-toggle (magit-current-section)))
-    ((string= mode-name "Shell")
-    (company-manual-begin))
-    (t
-    (indent-for-tab-command)
-    (if (or (not yas-minor-mode)
-            (null (do-yas-expand)))
-        (auto-complete)))))
-
-(use-package yasnippet
-  :ensure t
-  :init
-  (add-hook 'after-init-hook 'yas-global-mode)
-  (add-hook 'term-mode-hook #'force-yasnippet-off)
-  (add-hook 'shell-mode-hook #'force-yasnippet-off)
-  :config
-  (setq yas-snippet-dirs '("~/.emacs.d/snippets")))
-
-(yas-reload-all)
-
-(define-key yas-keymap [tab] 'mb/handle-tab)
-(define-key yas-keymap (kbd "TAB") 'mb/handle-tab)
-(bind-key* "TAB" 'mb/handle-tab)
-
-(yas-global-mode 1)
+(message "loading all the snippets")
+(add-hook 'prog-mode-hook #'yas-minor-mode)
 
 (provide 'yasnippet-settings)
