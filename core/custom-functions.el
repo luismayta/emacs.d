3fe50b9abafab8cafc2a8d20724bca88b76d09ef
@slovacus <slovacus@gmail.com>
2016-09-09 01:16:29 -0500

    [#80] Add: name function package-require

--- core/custom-functions.el
+++ core/custom-functions.el
@@ -40,10 +40,9 @@
   (add-to-list 'load-path (make-elget-path plugin)))
 
 ;; Functions install Package
-
-(defun bundle (depname refresh)
+(defun package-require (pkg)
   "Runs `package-install', attempting `package-refresh-contents' on failure."
-  (when refresh (package-refresh-contents))
-  (package-install depname))
+  (when (not (package-installed-p pkg))
+    (package-install pkg)))
 
 (provide 'custom-functions)
