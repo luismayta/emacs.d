f8feb0dbba33ddacb10bed4d914383282cfed7ba
@slovacus <slovacus@gmail.com>
2016-09-09 21:34:08 -0500

    [#81] Add: dependences externals

---
+++ brew.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# Install command-line tools using Homebrew.
+brew update
+brew upgrade --all
+
+# editor wide
+brew install the_silver_searcher
+brew install editorconfig
+brew install aspell --with-lang-en
+
+# for the GNU global tag system. Used by ggtags.
+brew install global
+
+brew install markdown
