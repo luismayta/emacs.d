e38d2645b9bf913d9fe02c9eb5adace9b4f4c262
@slovacus <slovacus@gmail.com>
2016-09-10 18:43:51 -0500

    fix: clean files settings to modules (#81)

--- settings/confmode-settings.el
+++
@@ -1,6 +0,0 @@
-
-(require 'conf-mode)
-
-(add-to-list 'auto-mode-alist '("\\.rc\\'" . conf-mode))
-
-(provide 'confmode-settings)
--- settings/sml-modeline-settings.el
+++
@@ -1,8 +0,0 @@
-;; Install and config packages for sml-modeline
-;;; code:
-
-(package-require 'sml-modeline)
-
-(sml-modeline-mode 1)
-
-(provide 'sml-modeline-settings)
