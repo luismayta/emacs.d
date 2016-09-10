7e3ea4b23374a57c5fd0e0e039232a0816092319
@slovacus <slovacus@gmail.com>
2016-09-09 22:03:32 -0500

    [#81] Add: code lm-defaults

---
+++ core/lm-defaults.el
@@ -0,0 +1,77 @@
+;;; lm-defaults ;;;
+;;; code:
+
+(require 'lm-defuns)
+
+;; Fix mac-port defaults.
+(when (equal system-type 'darwin)
+  (setq mac-option-modifier 'meta)
+  (setq mac-command-modifier 'super)
+  (global-set-key [(super v)] 'yank)
+  (global-set-key [(super q)] 'save-buffers-kill-emacs))
+
+;; Frame title formatting.
+(setq-default frame-title-format
+              '((:eval (if (buffer-file-name)
+                           (abbreviate-file-name (buffer-file-name))
+                         "%b"))))
+
+;; Character encodings default to utf-8.
+(prefer-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+
+;; Show column numbers in mode line.
+(setq column-number-mode t)
+
+;; Never ring the bell. Never.
+(setq ring-bell-function (lambda()))
+
+;; Don't disable any commands (e.g. `upcase-region').
+(setq disabled-command-function nil)
+
+;; Don't use dialog boxes.
+(setq use-dialog-box nil)
+
+;; Hide mouse while typing.
+(setq make-pointer-invisible t)
+
+;; Reduce keystroke echo delay.
+(setq echo-keystrokes 0.001)
+
+;; Enable y/n answers.
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Automatically scroll compilation window.
+(setq compilation-scroll-output 1)
+
+;; Disable annoying lock files.
+(setq create-lockfiles nil)
+
+;; Allow pasting selection outside of Emacs.
+(setq x-select-enable-clipboard t)
+
+;; Move files to trash when deleting
+(setq delete-by-moving-to-trash t)
+
+;; Ignore case for completion.
+(setq completion-ignore-case t
+      read-file-name-completion-ignore-case t)
+
+;; When copying something from outside emacs, save to kill-ring.
+(setq save-interprogram-paste-before-kill t)
+
+;; Dired defaults.
+(setq-default insert-directory-program "gls")
+(setq-default dired-listing-switches "-lhva")
+(setq-default dired-clean-up-buffers-too t)
+(setq-default dired-recursive-copies 'always)
+(setq-default dired-recursive-deletes 'top)
+
+;; Misc.
+(setq inhibit-startup-message t)
+(global-font-lock-mode t)
+
+(provide 'lm-defaults)
