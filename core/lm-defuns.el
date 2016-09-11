db749590639b059025eb2946f5bcd1a9ba280ed8
@slovacus <slovacus@gmail.com>
2016-09-11 01:45:40 -0500

    [#81] Add: functions project

--- core/lm-defuns.el
+++ core/lm-defuns.el
@@ -1,5 +1,4 @@
-;----------------------;
-;;; lm-defuns ;;;
+;;; init-defuns.el --- Custom functions required by other init files.
 
 ;;; code:
 ; unfill a paragraph, i.e., make it so the text does not wrap in the
@@ -43,5 +42,40 @@
   "Runs `package-install', attempting `package-refresh-contents' on failure."
   (when (not (package-installed-p pkg))
     (package-install pkg)))
+
+;; Function for determining emacs dir paths.
+(defun lm/emacs.d (path)
+  "Return path inside user's `.emacs.d'."
+  (expand-file-name path user-emacs-directory))
+
+(defun lm/cache-for (identifier)
+  "Return cache directory for given identifier."
+  (expand-file-name identifier (lm/emacs.d "var/cache")))
+
+(defun lm/mkdir-p (dir-path)
+  "Make directory if it doesn't exist."
+  (unless (file-exists-p dir-path)
+    (make-directory dir-path t)))
+
+(defun lm/load-directory (directory)
+  "Load recursively all `.el' files in DIRECTORY."
+  (dolist (element (directory-files-and-attributes directory nil nil nil))
+    (let* ((path (car element))
+           (fullpath (concat directory "/" path))
+           (isdir (car (cdr element)))
+           (ignore-dir (or (string= path ".") (string= path ".."))))
+      (cond
+       ((and (eq isdir t) (not ignore-dir))
+        (lm/load-directory fullpath))
+       ((and (eq isdir nil)
+             (string= (substring path -3) ".el")
+             (not (string-match "^\\." path)))
+        (load (file-name-sans-extension fullpath)))))))
+
+;; Add yasnippet support for company backends
+;; https://github.com/syl20bnr/spacemacs/pull/179
+(defun lm/backend-with-yas (backend)
+  (append (if (consp backend) backend (list backend))
+          '(:with company-yasnippet)))
 
 (provide 'lm-defuns)
