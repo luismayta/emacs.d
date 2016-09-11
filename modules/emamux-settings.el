f3bc2e9a9447c0946734226572a23c89a449e47e
@slovacus <slovacus@gmail.com>
2016-09-11 10:12:23 -0500

    [#81] Fix: clean files unnecesary

--- modules/emamux-settings.el
+++
@@ -1,7 +0,0 @@
-;; config emamux
-;;; code:
-(package-require 'emamux)
-
-(require 'emamux)
-
-(provide 'emamux-settings)
--- modules/s-settings.el
+++
@@ -1,7 +0,0 @@
-;; Install Packages
-;;; code:
-(package-require 's  nil)
-
-(require 's)
-
-(provide 's-settings)
