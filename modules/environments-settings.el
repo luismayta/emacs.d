a8652e2bd94faf6ae3ee7dbb4991649e6b7236f2
@slovacus <slovacus@gmail.com>
2016-09-11 11:26:43 -0500

    [#81] Clean: files modules unnecesary

--- modules/environments-settings.el
+++
@@ -1,7 +0,0 @@
-(package-require 'exec-path-from-shell)
-
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-env "GOPATH"))
-
-(provide 'environments-settings)
