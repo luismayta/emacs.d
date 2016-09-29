382f29d77b0993c9e0f36e773e9e2ebf7ad252e8
@slovacus <slovacus@gmail.com>
2016-09-29 10:18:40 -0500

    [#40] Fix: load script bootstrap

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
+
+# load source files externals
+if [ -e "$HOME/.pyenv" ]; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
+
+[ -r "$HOME/.gvm/scripts/gvm" ] && source "$HOME/.gvm/scripts/gvm"
+
+PROJECT_NAME=emacs
+PYTHON_VERSION=2.7.9
+PYENV_NAME="${PROJECT_NAME}"
+
+GVM_NAME="${PROJECT_NAME}"
+GVM_PATHS_NAME="{src, pkg, bin}"
+
+# Vars Dir
+export ROOT_DIR="`pwd`"
+export RESOURCES_DIR="$ROOT_DIR/resources"
+export SOURCE_DIR="$ROOT_DIR"
+export STATIC_DIR="$SOURCE_DIR/static/"
--- script/clean.sh
+++ script/clean.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+find $ROOT_DIR/ -name "*.pyc" -delete
+find $ROOT_DIR/ -name "*.swp" -delete
+find $ROOT_DIR/ -name "__pycache__" -delete
+
 # Directory
 rm -rf el-get/
 rm -rf elpa/
