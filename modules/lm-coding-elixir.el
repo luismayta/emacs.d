2f9fe8b5c62e96e5bba3ade0aa983eb1b1892dc4
@slovacus <slovacus@gmail.com>
2016-09-29 09:38:00 -0500

    [#87] Add: code comment

--- modules/lm-coding-elixir.el
+++ modules/lm-coding-elixir.el
@@ -1,8 +1,9 @@
 ;;; lm-coding-elixir.el --- Elixir configuration.
 
+;;; code:
 (use-package elixir-mode
   :mode (("\\.ex\\'" . elixir-mode)
-         ("\\.exs\\'" . elixir-mode))
+          ("\\.exs\\'" . elixir-mode))
   :config
   (company-quickhelp-mode -1)
   (use-package alchemist
