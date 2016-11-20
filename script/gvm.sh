cc00780e42f56994a521143f7529b0d406f88e2c
@slovacus <slovacus@gmail.com>
2016-11-20 00:12:47 -0500

    [#91] Fix: script makefile

--- script/gvm.sh
+++ script/gvm.sh
@@ -3,6 +3,8 @@
 
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
+[ -r "$HOME/.gvm/scripts/gvm" ] && source "$HOME/.gvm/scripts/gvm"
+
 gvm pkgset list | grep -q "${GVM_NAME}"
 response=$?
 
