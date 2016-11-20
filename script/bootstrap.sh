b3adbbd4b8e57a99af36fd9eae61a36d38f68085
@slovacus <slovacus@gmail.com>
2016-11-20 00:12:47 -0500

    [#91] Fix: script makefile

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# load source files externals
-if [ -e "$HOME/.pyenv" ]; then
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-fi
-
-[ -r "$HOME/.gvm/scripts/gvm" ] && source "$HOME/.gvm/scripts/gvm"
-
 PROJECT_NAME=emacs
+
 PYTHON_VERSION=2.7.9
 PYENV_NAME="${PROJECT_NAME}"
 
 GVM_NAME="${PROJECT_NAME}"
-GVM_PATHS_NAME="{src, pkg, bin}"
+GVM_PATHS_NAME=(
+    "src"
+    "pkg"
+    "bin"
+)
+
+GRIP_PORT=6430
 
 # Vars Dir
-export ROOT_DIR="`pwd`"
-export RESOURCES_DIR="$ROOT_DIR/resources"
-export SOURCE_DIR="$ROOT_DIR"
-export STATIC_DIR="$SOURCE_DIR/static/"
+ROOT_DIR="$(pwd)"
+RESOURCES_DIR="${SOURCE_DIR}/resources"
+RESOURCES_DB_DIR="${RESOURCES_DIR}/db"
+PROVISION_DIR="${ROOT_DIR}/provision/ansible"
+SOURCE_DIR="${ROOT_DIR}"
+REQUIREMENTS_DIR="${SOURCE_DIR}/requirements"
--- script/pyenv.sh
+++ script/pyenv.sh
@@ -3,6 +3,12 @@
 
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
+# load source files externals
+if [ -e "$HOME/.pyenv" ]; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
+
 cd "${ROOT_DIR}"
 
-pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" > /dev/null
+pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" >> /dev/null 2>&1
