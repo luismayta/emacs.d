b01605431eb98e3c6f3be4c10adcccf39a0903b2
@slovacus <slovacus@gmail.com>
2016-11-29 19:46:52 -0500

    [#97] Fix: add vars to exports

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-PROJECT_NAME=emacs
+export PROJECT_NAME=emacs
+export PYTHON_VERSION=2.7.9
+export PYENV_NAME="${PROJECT_NAME}"
 
-PYTHON_VERSION=2.7.9
-PYENV_NAME="${PROJECT_NAME}"
-
-GVM_NAME="${PROJECT_NAME}"
-GVM_PATHS_NAME=(
+export GVM_NAME="${PROJECT_NAME}"
+export GVM_PATHS_NAME=(
     "src"
     "pkg"
     "bin"
 )
 
-GRIP_PORT=6430
+export GRIP_PORT=6430
 
 # Vars Dir
-ROOT_DIR="$(pwd)"
-RESOURCES_DIR="${SOURCE_DIR}/resources"
-RESOURCES_DB_DIR="${RESOURCES_DIR}/db"
-PROVISION_DIR="${ROOT_DIR}/provision/ansible"
-SOURCE_DIR="${ROOT_DIR}"
-REQUIREMENTS_DIR="${SOURCE_DIR}/requirements"
+export ROOT_DIR="$(pwd)"
+export RESOURCES_DIR="${SOURCE_DIR}/resources"
+export RESOURCES_DB_DIR="${RESOURCES_DIR}/db"
+export PROVISION_DIR="${ROOT_DIR}/provision/ansible"
+export SOURCE_DIR="${ROOT_DIR}"
+export REQUIREMENTS_DIR="${SOURCE_DIR}/requirements"
