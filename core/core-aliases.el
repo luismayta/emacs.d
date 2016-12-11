6d5ddd4a43794679ffdb5666b741030e8338c588
@slovacus <slovacus@gmail.com>
2016-12-11 13:20:38 -0500

    fix: load module php (#100)

--- core/core-aliases.el
+++ core/core-aliases.el
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 ;;; core-aliases.el --- def vars.
 
-||||||| parent of e83bbce ([#100] Fix: change names)
-;;; lm-aliases.
-=======
-;;; core-aliases.
->>>>>>> e83bbce ([#100] Fix: change names)
 ;;; code:
 
 (defalias 'eb 'eval-buffer)
@@ -14,11 +8,4 @@
 
 (defalias 'rg 'rgrep)
 
-<<<<<<< HEAD
 (provide 'core-aliases)
-;;; core-aliases ends here
-||||||| parent of e83bbce ([#100] Fix: change names)
-(provide 'lm-aliases)
-=======
-(provide 'core-aliases)
->>>>>>> e83bbce ([#100] Fix: change names)
