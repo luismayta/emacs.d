0217a18273779787763b0de97b674a22b2a4b8e4
@slovacus <slovacus@gmail.com>
2016-12-11 13:50:30 -0500

    fix: load functions core (#100)

--- modules/module-coding-ruby.el
+++ modules/module-coding-ruby.el
@@ -1,15 +1,15 @@
-;;; lm-coding-ruby.el --- Ruby configuration.
+;;; module-coding-ruby.el --- Ruby configuration.
 
 (use-package ruby-mode
   :ensure ruby-tools
   :ensure inf-ruby
   :interpreter "ruby"
   :mode (("Fastfile$" . ruby-mode)
-         ("Appfile$" . ruby-mode))
+          ("Appfile$" . ruby-mode))
   :config
   (add-hook 'ruby-mode-hook
-            (lambda ()
-              (inf-ruby-minor-mode t)
-              (ruby-tools-mode t))))
+    (lambda ()
+      (inf-ruby-minor-mode t)
+      (ruby-tools-mode t))))
 
-(provide 'lm-coding-ruby)
+(provide 'module-coding-ruby)
