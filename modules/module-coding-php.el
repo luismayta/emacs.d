bdee7b6e70ef7dd33b75e3c01aff4f8102be2aac
@slovacus <slovacus@gmail.com>
2016-12-11 13:20:38 -0500

    [#100] Fix: load module php

--- modules/module-coding-php.el
+++ modules/module-coding-php.el
@@ -1,15 +1,18 @@
-;;; lm-coding-php.el --- Php settings.
+;;; module-coding-php.el --- Php settings.
 ;;; code:
 
 (use-package php-mode
   :commands php-mode
   :ensure php-mode
+  :config
+  (add-hook 'php-mode-hook #'ggtags-mode)
   :mode
   (("\\.phtml\\" . php-mode)
+    ("\\.php[345]?\\" . php-mode)
     ("\\.php\\" . php-mode)
     ("\\.inc\\" . php-mode)))
 
 ;; company-php
 (use-package company-php)
 
-(provide 'lm-coding-php)
+(provide 'module-coding-php)
