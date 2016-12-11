274c2afa180c3387ca83372bd6f5e4d0b8787156
@slovacus <slovacus@gmail.com>
2016-12-11 13:20:38 -0500

    fix: load module php (#100)

--- modules/module-coding-web.el
+++ modules/module-coding-web.el
@@ -1,20 +1,15 @@
-;;; lm-coding-web.el --- web related coding configuration.
-
-(use-package php-mode
-  :mode "\\.php[345]?\\'"
-  :config
-  (add-hook 'php-mode-hook #'ggtags-mode))
+;;; module-coding-web.el --- web related coding configuration.
 
 (use-package web-mode
   :mode (("\\.html\\'" . web-mode)
-        ("\\.phtml\\'" . web-mode)
-        ("\\.tpl\\'" . web-mode)
-        ("\\.[agj]sp\\'" . web-mode)
-        ("\\.as[cp]x\\'" . web-mode)
-        ("\\.erb\\'" . web-mode)
-        ("\\.mustache\\'" . web-mode)
-        ("\\.djhtml\\'" . web-mode)
-        ("\\.jade\\'" . web-mode)
-        ("\\.json\\'" . web-mode)))
+          ("\\.phtml\\'" . web-mode)
+          ("\\.tpl\\'" . web-mode)
+          ("\\.[agj]sp\\'" . web-mode)
+          ("\\.as[cp]x\\'" . web-mode)
+          ("\\.erb\\'" . web-mode)
+          ("\\.mustache\\'" . web-mode)
+          ("\\.djhtml\\'" . web-mode)
+          ("\\.jade\\'" . web-mode)
+          ("\\.json\\'" . web-mode)))
 
-(provide 'lm-coding-web)
+(provide 'module-coding-web)
