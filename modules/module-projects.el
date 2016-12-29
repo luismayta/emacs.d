7c629cc748747df849b92c4a369270320a1c610c
@slovacus <slovacus@gmail.com>
2016-12-29 01:30:05 -0500

    [#95] Fix: settings module projects

--- modules/module-projects.el
+++ modules/module-projects.el
@@ -1,5 +1,15 @@
 ;;; module-projects.el --- Project management         -*- lexical-binding: t; -*-
 ;;; code:
+(use-package helm
+  :init
+  (helm-mode t)
+  :config
+  (global-set-key (kbd "C-c h") 'helm-mini))
+
+(use-package helm-descbinds
+  :config
+  (fset 'describe-bindings 'helm-descbinds))
+
 ;; Add: info helm-ag
 (use-package helm-ag)
 
@@ -9,11 +19,12 @@
 
 (use-package projectile
   :diminish projectile-mode
-  :commands
-  (projectile-mode projectile-global-mode)
+  :commands  (projectile-mode
+               projectile-global-mode)
   :bind ("C-c p a" . projectile-ag)
   :init
   (add-hook 'after-init-hook 'projectile-global-mode)
+  (add-hook 'after-init-hook 'helm-projectile-on)
   :config
   (setq projectile-completion-system 'helm)
   (setq projectile-file-exists-local-cache-expire (* 5 60)))
@@ -29,5 +40,5 @@
     (when (< (+ 1 (persp-curr-position)) (length (persp-all-names)))
       (persp-switch (nth (1+ (persp-curr-position)) (persp-all-names))))))
 
-(provide 'module-project)
+(provide 'module-projects)
 ;;; module-projects.el ends here
