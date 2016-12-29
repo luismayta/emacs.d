0e667175a583aabf199927a88e6ff1bcc6ee5bd9
@slovacus <slovacus@gmail.com>
2016-12-29 13:11:27 -0500

    [#98] Fix: load file script

--- script/pyenv.sh
+++ script/pyenv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# shellcheck source=script/bootstrap.sh
+# shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 # load source files externals
@@ -12,4 +12,4 @@ fi
 
 cd "${ROOT_DIR}" || exit
 
-pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" >> /dev/null 2>&1 || echo 'Yeah!!'
+pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" >> /dev/null 2>&1 || echo 'Oh Yeah!!'
