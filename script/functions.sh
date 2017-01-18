04f636447d39f8c6de274970b9481609b0691851
@slovacus <slovacus@gmail.com>
2017-01-18 01:10:36 -0500

    [#101] Fix: load functions

--- script/functions.sh
+++ script/functions.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
 msg() {
     printf '%b\n' "$1" >&2
 }
 
 success() {
-    if [ "$ret" -eq '0' ]; then
-        msg "\e[32m[✔]\e[0m ${1}${2}"
-    fi
+    msg "\e[32m[✔]\e[0m ${1}${2}"
 }
 
 error() {
