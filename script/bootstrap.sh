b3adb1b80149a994b755a4a25709fc932f3c79c9
@slovacus <slovacus@gmail.com>
2017-01-18 01:09:49 -0500

    [#101] Fix: load functions in bootstrap

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -15,9 +15,16 @@ export GVM_PATHS_NAME=(
 export GRIP_PORT=6430
 
 # Vars Dir
-export ROOT_DIR="$(pwd)"
+
+export ROOT_DIR
+ROOT_DIR=$(pwd)
 export RESOURCES_DIR="${SOURCE_DIR}/resources"
 export RESOURCES_DB_DIR="${RESOURCES_DIR}/db"
 export PROVISION_DIR="${ROOT_DIR}/provision/ansible"
 export SOURCE_DIR="${ROOT_DIR}"
+export SCRIPT_DIR="${SOURCE_DIR}/script"
 export REQUIREMENTS_DIR="${SOURCE_DIR}/requirements"
+
+
+# shellcheck source=/dev/null
+[ -r "${SCRIPT_DIR}/functions.sh" ] && source "${SCRIPT_DIR}/functions.sh"
