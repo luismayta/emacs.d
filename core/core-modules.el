8b7a3a9ff1327960e6e08d7742c6d9ff16263fba
@slovacus <slovacus@gmail.com>
2017-05-01 16:18:02 -0500

    !#106 Fix: settings emacs unnecesary

--- core/core-modules.el
+++ core/core-modules.el
@@ -27,8 +27,8 @@
      module-appearance
      module-snippets
      module-searching
-     module-navigation
      module-writing
+     module-navigation
      module-projects
      module-source-control
      module-coding-general
