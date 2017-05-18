d4ef781de2fdea720b6e609c05721b721df2f402
@slovacus <slovacus@gmail.com>
2017-05-17 19:01:17 -0500

    !#122 Add: vars font and path

--- core/core-vars.el
+++ core/core-vars.el
@@ -1,10 +1,16 @@
 ;;; core-vars.el --- def vars.
 
 ;;; code:
+
 (defvar core/fixed-font-name "Fira Code")
 (defvar core/fixed-font-weight 'normal)
 (defvar core/var-font-name "Fira Code")
-(defvar core/font-height 130)
+(defvar core/font-height 150)
+
+(defvar private-dir  (expand-file-name "private" user-emacs-directory))
+(defvar temp-dir
+  (format "%s/cache" private-dir)
+  "Hostname-based elisp temp directories.")
 
 ;; vars for terminals.
 (defvar my-term-shell "/bin/zsh")
