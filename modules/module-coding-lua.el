3fb53c6e35c3033eec9f8630d31e5b93ab6fdeeb
@slovacus <slovacus@gmail.com>
2017-05-23 21:25:22 -0500

    !#124 Add: code module lua

---
+++ modules/module-coding-lua.el
@@ -0,0 +1,9 @@
+;;; module-coding-lua.el --- lua settings.
+;;; code:
+(use-package lua-mode
+  :mode
+  ((("\\.lua$" . lua-mode)
+     ("\\.lua" . lua-mode))))
+
+(provide 'module-coding-lua)
+;;; module-coding-lua.el ends here
