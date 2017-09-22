7837389f30e93cafa2fd53b2559cc98a6fe6eb3f
@slovacus <slovacus@gmail.com>
2017-09-22 02:42:11 -0500

    !#120 Fix: settings module python

--- modules/module-coding-python.el
+++ modules/module-coding-python.el
@@ -2,18 +2,20 @@
 ;;; code:
 (use-package elpy
   :commands
-  (elpy-enable)
+  (elpy-enable elpy-use-ipython)
+  :mode ("\\.py$" . elpy-mode)
   :init
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (add-hook 'elpy-mode-hook 'flycheck-mode)
+  (add-hook 'python-mode-hook 'elpy-mode))
 
 ;; Install pyenv-mode-auto package
 (use-package pyenv-mode-auto
   :init
   (pyenv-mode))
 
-;; company-jedi
-(use-package company-jedi
-  :ensure t)
+(use-package py-autopep8
+  :init
+  (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
 
 ;; (when (require 'flycheck nil t)
 ;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
