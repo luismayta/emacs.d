16a52dd38811137c8c620e643b231622cb9536bc
@slovacus <slovacus@gmail.com>
2017-11-15 08:52:41 -0500

    !#151 Fix: clean code

--- modules/module-company.el
+++ modules/module-company.el
@@ -34,18 +34,7 @@
 (use-package company
   :ensure t
   :defer t
-  :init (global-company-mode)
-  :config
-  (progn
-    ;; Use Company for completion
-    (bind-key [remap completion-at-point] #'company-complete company-mode-map)
-
-    (setq company-tooltip-align-annotations t
-      ;; Easy navigation to candidates with M-<n>
-      company-show-numbers t)
-    (setq company-dabbrev-downcase nil))
-  :diminish company-mode)
-
+  :init (global-company-mode))
 
 (use-package company-quickhelp          ; Documentation popups for Company
   :ensure t
