4063a954e2d08f7f64f8056996fd0b334b966c0e
@slovacus <slovacus@gmail.com>
2017-11-18 23:03:06 -0500

    chore: var settings env (#156)

--- modules/module-path.el
+++ modules/module-path.el
@@ -1,17 +1,19 @@
 ;; Use $PATH from user's shell in Emacs.
 ;; code:
 
+
 (use-package exec-path-from-shell
   :config
-  (exec-path-from-shell-copy-env "PATH")
-  (exec-path-from-shell-copy-env "GOPATH")
-  (exec-path-from-shell-copy-env "GOROOT")
-  (exec-path-from-shell-copy-env "GO_ENV")
-  (exec-path-from-shell-copy-env "DOCKER_HOST")
-  (exec-path-from-shell-copy-env "DOCKER_TLS_VERIFY")
-  (exec-path-from-shell-copy-env "DOCKER_CERT_PATH")
-  (exec-path-from-shell-copy-env "DOCKER_MACHINE_NAME")
-  (exec-path-from-shell-copy-env "PLANTUML_JAR_PATH")
+  (setq exec-path-from-shell-copy-env "PATH")
+  (setq exec-path-from-shell-copy-env "GOPATH")
+  (setq exec-path-from-shell-copy-env "GOROOT")
+  (setq exec-path-from-shell-copy-env "GO_ENV")
+  (setq exec-path-from-shell-copy-env "DOCKER_HOST")
+  (setq exec-path-from-shell-copy-env "DOCKER_TLS_VERIFY")
+  (setq exec-path-from-shell-copy-env "DOCKER_CERT_PATH")
+  (setq exec-path-from-shell-copy-env "DOCKER_MACHINE_NAME")
+  (setq exec-path-from-shell-copy-env "PLANTUML_JAR_PATH")
+  (setq exec-path-from-shell-check-startup-files nil)
   (exec-path-from-shell-initialize))
 
 (provide 'module-path)
