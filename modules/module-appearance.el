9103a7bf8c01722e3b94f9ed5bd307310c766560
@slovacus <slovacus@gmail.com>
2017-11-19 20:20:47 -0500

    !#156 Fix: best apparance startup emacs

--- modules/module-appearance.el
+++ modules/module-appearance.el
@@ -3,31 +3,82 @@
 ;;; code:
 
 (require 'core-vars)
+(require 'core-defuns)
 
-;; Disable cursor display in inactive windows.
-(setq-default cursor-in-non-selected-windows nil)
+(use-package no-littering)
+(use-package use-package-chords)
+(use-package use-package-ensure-system-package)
+(use-package add-hooks)
 
-(add-hook 'emacs-startup-hook ;; 'window-setup-hook
-  (lambda nil
-    (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-    (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-    (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-    (run-with-idle-timer 0.1 nil (lambda nil (toggle-frame-maximized)))
-    (set-face-attribute
-      'default nil
-      :family core/fixed-font-name
-      :height core/font-height
-      :weight core/fixed-font-weight)
-    (set-face-attribute
-      'linum nil
-      :family core/fixed-font-name
-      :height core/font-height
-      :weight core/fixed-font-weight)
-    (set-face-attribute
-      'variable-pitch nil
-      :family core/var-font-name))
+(use-package pretty-mode
+  :ensure t
+  :config
+  (global-pretty-mode t)
+  (global-prettify-symbols-mode t)
+  (pretty-deactivate-groups
+    '(:logic :sets))
+  (pretty-activate-groups
+    '(:equality :ordering :ordering-double :ordering-triple
+       :arrows :arrows-twoheaded :punctuation
+       :sub-and-superscripts :greek :arithmetic-nary)))
+
+
+(def lm-install-fira-code-font
+  (let* ((font-name "FiraCode-Retina.ttf")
+          (font-url
+            (format "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/%s?raw=true" font-name))
+          (font-dest
+            (cl-case window-system
+              (x  (concat (or (getenv "XDG_DATA_HOME")
+                            (concat (getenv "HOME") "/.local/share"))
+                    "/fonts/"))
+              (mac (concat (getenv "HOME") "/Library/Fonts/" ))
+              (ns (concat (getenv "HOME") "/Library/Fonts/" )))))
+    (unless (file-directory-p font-dest) (mkdir font-dest t))
+    (url-copy-file font-url (expand-file-name font-name font-dest) t)
+    (message "Fonts downloaded, updating font cache... <fc-cache -f -v> ")
+    (shell-command-to-string (format "fc-cache -f -v"))
+    (message "Successfully install `fira-code' font to `%s'!" font-dest)))
+
+(def lm-setup-fira-code-font
+  ;; (unless (member "Fira Code" (font-family-list))
+  ;;   (lm-install-fira-code-font))
+  (set-frame-font "Fira Code Retina-17"))
+;; (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+;;                 (35 . ".\\(?:###\\|##\\|[#(?[_{]\\)")
+;;                 (36 . ".\\(?:>\\)")
+;;                 (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+;;                 (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+;;                 (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+;;                 (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+;;                 ;; (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+;;                 ;; (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+;;                 ;; (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+;;                 (48 . ".\\(?:x[a-zA-Z]\\)")
+;;                 (58 . ".\\(?:::\\|[:=]\\)")
+;;                 (59 . ".\\(?:;;\\|;\\)")
+;;                 (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+;;                 (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+;;                 (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+;;                 (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+;;                 (91 . ".\\(?:]\\)")
+;;                 (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+;;                 (94 . ".\\(?:=\\)")
+;;                 (119 . ".\\(?:ww\\)")
+;;                 (123 . ".\\(?:-\\)")
+;;                 (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+;;                 (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)"))))
+;;   (dolist (char-regexp alist)
+;;     (set-char-table-range composition-function-table (car char-regexp)
+;;       `([,(cdr char-regexp) 0 font-shape-gstring])))))
+
+(def setup-windows-hook
+  (run-with-idle-timer 0.1 nil (lambda nil (toggle-frame-maximized)))
+  (lm-setup-fira-code-font)
   (use-package zerodark-theme
     :config (load-theme 'zerodark 1)))
+
+(add-hook 'emacs-startup-hook #'setup-windows-hook)
 
 (use-package nyan-mode
   :init
