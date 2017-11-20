a5f9db0c99a081f7864b24839f15f9f8f3cca451
@slovacus <slovacus@gmail.com>
2017-11-20 18:21:53 -0500

    !#156 Fix: order packages for appearance

--- modules/module-appearance.el
+++ modules/module-appearance.el
@@ -22,6 +22,9 @@
        :arrows :arrows-twoheaded :punctuation
        :sub-and-superscripts :greek :arithmetic-nary)))
 
+(use-package nyan-mode
+  :init
+  (nyan-mode))
 
 (def lm-install-fira-code-font
   (let* ((font-name "FiraCode-Retina.ttf")
@@ -43,46 +46,62 @@
 (def lm-setup-fira-code-font
   ;; (unless (member "Fira Code" (font-family-list))
   ;;   (lm-install-fira-code-font))
-  (set-frame-font "Fira Code Retina-17"))
-;; (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
-;;                 (35 . ".\\(?:###\\|##\\|[#(?[_{]\\)")
-;;                 (36 . ".\\(?:>\\)")
-;;                 (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
-;;                 (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
-;;                 (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
-;;                 (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
-;;                 ;; (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
-;;                 ;; (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
-;;                 ;; (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
-;;                 (48 . ".\\(?:x[a-zA-Z]\\)")
-;;                 (58 . ".\\(?:::\\|[:=]\\)")
-;;                 (59 . ".\\(?:;;\\|;\\)")
-;;                 (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
-;;                 (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
-;;                 (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
-;;                 (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
-;;                 (91 . ".\\(?:]\\)")
-;;                 (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
-;;                 (94 . ".\\(?:=\\)")
-;;                 (119 . ".\\(?:ww\\)")
-;;                 (123 . ".\\(?:-\\)")
-;;                 (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
-;;                 (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)"))))
-;;   (dolist (char-regexp alist)
-;;     (set-char-table-range composition-function-table (car char-regexp)
-;;       `([,(cdr char-regexp) 0 font-shape-gstring])))))
+  ;; Fira Code enabled
+  ;; https://github.com/tonsky/FiraCode/wiki/Setting-up-Emacs
+  (when (window-system)
+    (set-frame-font "Fira Code Retina-17"))
+  (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+                  (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
+                  (36 . ".\\(?:>\\)")
+                  (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+                  (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+                  (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+                  (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+                  (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+                  (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+                  (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+                  (48 . ".\\(?:x[a-zA-Z]\\)")
+                  (58 . ".\\(?:::\\|[:=]\\)")
+                  (59 . ".\\(?:;;\\|;\\)")
+                  (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+                  (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+                  (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+                  (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+                  (91 . ".\\(?:]\\)")
+                  (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+                  (94 . ".\\(?:=\\)")
+                  (119 . ".\\(?:ww\\)")
+                  (123 . ".\\(?:-\\)")
+                  (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+                  (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
+                  )
+          ))
+    (dolist (char-regexp alist)
+      (set-char-table-range composition-function-table (car char-regexp)
+        `([,(cdr char-regexp) 0 font-shape-gstring])))))
 
 (def setup-windows-hook
   (run-with-idle-timer 0.1 nil (lambda nil (toggle-frame-maximized)))
-  (lm-setup-fira-code-font)
+  (when (eq system-type 'darwin)
+    (setq mac-allow-anti-aliasing t)  ;; nice fonts in OS X
+    (set-face-attribute 'default nil :height 135))
+  (when (eq system-type 'gnu/linux)
+    (set-face-attribute 'default nil :height 116))
+                                        ;(lm-setup-fira-code-font)
+  (when (window-system)
+    (set-frame-font "Fira Code Retina-17"))
+  (setq-default truncate-lines 1)  ;; no word wrap
+  (setq-default line-spacing 4)
+  (use-package all-the-icons)
   (use-package zerodark-theme
-    :config (load-theme 'zerodark 1)))
+    :config
+    (load-theme 'zerodark 1)
+    ;; (zerodark-setup-modeline-format)
+    (setq zerodark-use-paddings-in-mode-line nil)
+    (set-cursor-color "violet")
+    ))
 
 (add-hook 'emacs-startup-hook #'setup-windows-hook)
-
-(use-package nyan-mode
-  :init
-  (nyan-mode))
 
 (use-package rainbow-mode
   :commands rainbow-mode)
@@ -94,10 +113,9 @@
 (use-package hl-line
   :config
   ;; Doesn't seem to play nice in emacs 25+
-  (setq hl-line-sticky-flag nil
-    global-hl-line-sticky-flag nil)
-
   (defvar-local current-hl-line-mode nil)
+  (setq hl-line-sticky-flag nil)
+  (setq global-hl-line-sticky-flag nil)
   (defun hl-line-on ()  (if current-hl-line-mode (hl-line-mode +1)))
   (defun hl-line-off () (if current-hl-line-mode (hl-line-mode -1)))
   ;;(add-hook hl-line-mode (lambda () (if current-hl-line-mode (setq current-hl-line-mode t))))
@@ -109,7 +127,7 @@
 
 (use-package linum
   :config
-  (setq linum-format " %4d ")
+  (setq linum-format " %3d ")
   (global-linum-mode nil)
   :init
   (add-hook 'prog-mode-hook #'linum-mode))
