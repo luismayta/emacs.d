85c47a2b7d2bc34da81109a78509732ea29e4d09
@slovacus <slovacus@gmail.com>
2017-11-25 23:01:08 -0500

    !#162 Add: package install git hg

--- brew.sh
+++ brew.sh
@@ -5,6 +5,7 @@
 brew update
 
 # editor wide
+brew install git hg
 brew install the_silver_searcher
 brew install editorconfig
 brew install aspell --with-lang-en
@@ -12,6 +13,7 @@ brew install aspell --with-lang-en
 # for the GNU global tag system. Used by ggtags.
 brew install --HEAD ctags
 brew install global --with-ctags
+
 # program used for plantuml
 brew install graphviz
 
