2694a5286d3a06b79ba1ba868a3983e12c320985
@slovacus <slovacus@gmail.com>
2017-11-25 22:13:18 -0500

    !#162 Fix: settings packages python

--- modules/module-coding-python.el
+++ modules/module-coding-python.el
@@ -4,48 +4,56 @@
 (require 'core-defuns)
 
 (use-package python-mode
-  :mode ("\\.py$" . python-mode))
+  :mode ("\\.py\\'" . python-mode)
+  :config
+  (use-package python-docstring
+    :ensure t
+    :init
+    (add-hook 'python-mode-hook 'python-docstring-mode))
+  ;; Install pyenv-mode-auto package
+  (use-package pyenv-mode-auto
+    :init
+    (pyenv-mode))
+  )
 
-(use-package python-docstring
-  :ensure t
-  :init
-  (add-hook 'python-mode-hook 'python-docstring-mode))
+(use-package pip-requirements
+  :config
+  (add-hook 'pip-requirements-mode-hook #'pip-requirements-auto-complete-setup)
+  )
 
 (use-package elpy
   :ensure t
-  :init
-  (elpy-enable)
   :config
+  (setq elpy-rpc-backend "jedi")
+  (elpy-enable)
   (elpy-use-ipython)
-  (setq python-shell-interpreter "ipython"
-    python-shell-interpreter-args "--simple-prompt --pprint")
-
   (when (require 'flycheck nil t)
     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
     (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (setq python-shell-interpreter "ipython"
+    python-shell-interpreter-args "--simple-prompt --pprint"))
+;; (add-hook 'python-mode-hook
+;;   (lambda ()
+;;     (add-to-list (make-local-variable 'company-backends)
+;;       '(elpy-company-backend)))))
 
-  (add-hook 'python-mode-hook
-    (lambda ()
-      (add-to-list (make-local-variable 'company-backends)
-        '(elpy-company-backend))))
-  (add-hook 'inferior-python-mode-hook
-    (lambda ()
-      (push
-        'comint-watch-for-password-prompt comint-output-filter-functions)))
-  )
+(use-package jedi
+  :ensure t)
 
-;; Install pyenv-mode-auto package
-(use-package pyenv-mode-auto
+(use-package jedi-direx
+  :ensure t)
+
+(use-package company-jedi
   :init
-  (pyenv-mode))
+  (progn
+    (add-hook 'python-mode-hook
+      (lambda ()
+        (add-to-list (make-local-variable 'company-backends)
+          '(company-jedi))))))
 
 (use-package py-autopep8
   :init
   (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
-
-;; (when (require 'flycheck nil t)
-;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-;;   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 (provide 'module-coding-python)
 ;;; module-coding-python.el ends here
