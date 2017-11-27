20c06f27aac8406fb1ae0cd0109930ef3a6e3cd1
@slovacus <slovacus@gmail.com>
2017-11-26 21:08:25 -0500

    !#162 Add: vars fonts weight

--- core/core-vars.el
+++ core/core-vars.el
@@ -4,17 +4,53 @@
 
 (defvar core/fixed-font-name "Fira Code")
 (defvar core/frame-font "Fira Code Retina-15")
-(defvar core/fixed-font-weight 'normal)
+(defvar core/fixed-font-weight "normal")
 (defvar core/var-font-name "Fira Code")
 (defvar core/font-height 170)
 
-(defvar private-dir  (expand-file-name "private" user-emacs-directory))
+;; vars for terminals.
+(defvar my-term-shell "/usr/local/bin/zsh")
+
+;; paths
+(defvar start-directory
+  user-emacs-directory
+  "Start directory.")
+
+(defconst private-directory
+  (expand-file-name (concat start-directory "private/"))
+  "Core directory.")
+
 (defvar temp-dir
-  (format "%s/cache" private-dir)
+  (format "%s/cache" private-directory)
   "Hostname-based elisp temp directories.")
 
-;; vars for terminals.
-(defvar my-term-shell "/bin/zsh")
+(defconst core-directory
+  (expand-file-name (concat start-directory "core/"))
+  "Core directory.")
+
+(defconst modules-directory
+  (expand-file-name (concat start-directory "modules/"))
+  "Modules directory.")
+
+(defconst elisp-directory
+  (expand-file-name (concat start-directory "lib/elisp"))
+  "Elisp directory.")
+
+(defconst themes-directory
+  (expand-file-name (concat start-directory "lib/color-themes"))
+  "Themes directory.")
+
+(defconst test-directory
+  (expand-file-name (concat start-directory "tests/"))
+  "Tests directory.")
+
+(defconst cache-directory
+  (expand-file-name (concat start-directory "var/cache"))
+  "Cache directory.")
+
+(defconst exec-path
+  (expand-file-name "/usr/local/bin")
+  "Exec path directory.")
 
 (provide 'core-vars)
 ;;; core-vars.el ends here
