2169e61c93b988e7209f144b531afed6ab083c98
@slovacus <slovacus@gmail.com>
2017-11-26 21:06:59 -0500

    !#162 Add: add to list path

--- core/core-load-paths.el
+++ core/core-load-paths.el
@@ -1,43 +1,16 @@
 ;;; core-load-paths.el --- def vars.
-
 ;;; code:
-(defun add-to-load-path (dir) (add-to-list 'load-path dir))
+
+(add-to-list 'exec-path "/usr/local/bin")
+
+(defun add-to-load-path (dir) (add-to-list 'load-path dir)
+  )
 
 (defun add-to-load-path-if-exists (dir)
   "If DIR exists in the file system, add it to `load-path'."
   (when (file-exists-p dir)
-    (add-to-load-path dir)))
-
-;; paths
-(defvar start-directory
-  user-emacs-directory
-  "Start directory.")
-
-(defconst core-directory
-  (expand-file-name (concat start-directory "core/"))
-  "Core directory.")
-
-(defconst modules-directory
-  (expand-file-name (concat start-directory "modules/"))
-  "Modules directory.")
-
-(defconst elisp-directory
-  (expand-file-name (concat start-directory "lib/elisp"))
-  "Elisp directory.")
-
-(defconst themes-directory
-  (expand-file-name (concat start-directory "lib/color-themes"))
-  "Themes directory.")
-
-(defconst test-directory
-  (expand-file-name (concat start-directory "tests/"))
-  "Tests directory.")
-
-(defconst exec-path
-  (expand-file-name "/usr/local/bin")
-  "Exec path directory.")
-
-(add-to-list 'exec-path "/usr/local/bin")
+    (add-to-load-path dir))
+  )
 
 ;; load paths
 (mapc 'add-to-load-path
