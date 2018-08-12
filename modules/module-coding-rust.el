aa20a6aebeb413f2464d196479d59fdb29745d2a
@slovacus <slovacus@gmail.com>
2018-08-11 19:00:58 -0500

    (#173) Add: settings coding rust enabled

--- modules/module-coding-rust.el
+++ modules/module-coding-rust.el
@@ -1,8 +1,23 @@
 ;;; module-coding-rust.el --- Rust settings.
 ;;; code:
-(use-package rust-mode
-  :init
-  (rust-mode t))
 
-(provide 'module-coding-rust)
+(use-package rust-mode
+  :ensure t
+  :defer t
+  :config
+  (use-package company-racer
+    :ensure t)
+  (use-package cargo
+    :ensure t)
+  (use-package flycheck-rust
+    :ensure t
+    :config
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+  (use-package racer
+    :ensure t
+    :config
+    (add-hook 'rust-mode-hook #'racer-mode)
+    (add-hook 'racer-mode-hook #'eldoc-mode)))
+
+    (provide 'module-coding-rust)
 ;;; module-coding-rust.el ends here
