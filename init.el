c91618708867978e43136557f8cc4a7d0fe8ce21
@slovacus <slovacus@gmail.com>
2018-10-30 02:52:19 -0500

    (1.6.3) Add: package list dependences

--- init.el
+++ init.el
@@ -2,6 +2,27 @@
 
 ;;; code:
 (package-initialize)
+(add-to-list 'package-archives
+             '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives 
+  '("melpa" . "https://melpa.org/packages/") t )
+;(add-to-list 'package-archives
+;  '("melpa-mirror" . "https://www.mirrorservice.org/sites/melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives 
+             '("org" . "http://orgmode.org/elpa/") t)
+
+;;("melpa-stable". "http://stable.melpa.org/packages/")
+
+(unless package-archive-contents
+(package-refresh-contents)
+;; Ensure `use-package' is installed.
+(when (not (package-installed-p 'use-package))
+(package-install 'use-package)
+(setq use-package-always-ensure t))
+)
+
 
 ;; Increase the GC threshold as soon as possible.
 (setq gc-cons-threshold 50000000)
