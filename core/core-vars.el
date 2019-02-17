9e91c331ae329a6d9345307d3353af5c941bd5c0
@slovacus <slovacus@gmail.com>
2019-02-16 21:46:13 -0500

    fix: change format font (#177)

--- core/core-vars.el
+++ core/core-vars.el
@@ -3,8 +3,8 @@
 ;;; code:
 
 (defvar core/fixed-font-name "Fira Code")
-(defvar core/frame-font "Fira Code Retina-15")
-(defvar core/default-font "Fira Code Retina-16")
+(defvar core/frame-font "Fira Code Retina-17")
+(defvar core/default-font "Fira Code Retina-17")
 (defvar core/fixed-font-weight "normal")
 (defvar core/var-font-name "Fira Code")
 (defvar core/font-height 170)
