887834a23a383f61df78d05addaddefd22340748
@slovacus <slovacus@gmail.com>
2019-02-16 21:41:32 -0500

    fix: actions for make (#177)

--- extras/make/docs.mk
+++ extras/make/docs.mk
@@ -5,15 +5,17 @@
 FILE_README=$(ROOT_DIR)/README.rst
 PATH_DOCKER_COMPOSE:=provision/docker-compose
 
+docs: docs.help
+
 docs.help:
 	@echo '    Docs:'
 	@echo ''
 	@echo '        docs.show                  Show restview README'
-	@echo '        docs.make.html             Make documentation html'
+	@echo '        docs.make                  Make documentation html'
 	@echo ''
 
 docs.show: clean
 	restview "${FILE_README}"
 
-docs.make.html: clean
+docs.make: clean
 	$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml run --rm docs bash -c "cd docs && make html"
