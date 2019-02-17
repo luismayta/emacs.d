0ca42b1ca98a572a247dba4f560ff4e39986c32c
@slovacus <slovacus@gmail.com>
2019-02-16 21:38:52 -0500

    (#177) Fix: clean unnecesary files

--- modules/module-coding-terraform.el
+++
@@ -1,8 +0,0 @@
-;;; module-coding-terraform.el --- Terraform settings.
-;;; code:
-
-(use-package terraform-mode
-  :mode ("\\.inc\\'" . terraform-mode))
-
-(provide 'module-coding-terraform)
-;;; module-coding-terraform.el ends here
