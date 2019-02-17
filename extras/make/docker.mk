12285ad0fdda01202fd3cb9df7d6ce9c470e7f2c
@slovacus <slovacus@gmail.com>
2019-02-16 21:41:32 -0500

    (#177) Fix: actions for make

--- extras/make/docker.mk
+++ extras/make/docker.mk
@@ -8,66 +8,66 @@ docker: docker.help
 docker.help:
 	@echo '    Docker:'
 	@echo ''
-	@echo '        docker.build         build all services with docker-compose optional args=(--pull|...)'
+	@echo '        docker.build         build all or one example: make docker.build service={{services}}  args=(--pull|...)'
 	@echo '        docker.down          down services docker-compose'
 	@echo '        docker.exec          exec command in container by {{services}} {{command}}'
 	@echo '        docker.ssh           connect by ssh to container'
-	@echo '        docker.stop          stop services by env'
+	@echo '        docker.stop          stop services by stage'
 	@echo '        docker.verify_network           verify network'
 	@echo '        docker.up             up services of docker-compose'
-	@echo '        docker.run            run {service} {env}'
-	@echo '        docker.restart        restart by {env}'
+	@echo '        docker.run            run {service} {stage}'
+	@echo '        docker.restart        restart by {stage}'
 	@echo '        docker.list           list services of docker'
 	@echo ''
 
 docker.run: clean
-	@if [ -z "${env}" ]; then \
+	@if [ -z "${stage}" ]; then \
 		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml run --rm ${service} bash; \
 	else \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${env}".yml run --rm ${service} bash; \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml run --rm ${service} bash; \
 	fi
 
 docker.restart: clean
-	@if [ "${env}" == "" ]; then \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml --project-name "${PROJECT}" restart; \
+	@if [ "${stage}" == "" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml restart; \
 	else \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${env}".yml --project-name "${PROJECT}-${env}" restart; \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml restart; \
 	fi
 
 docker.build: clean
-	@echo $(MESSAGE) "Building environment: ${env}"
-	@if [ "${env}" == "" ]; then \
-		docker volume create "${PROJECT}"-db; \
-		docker volume create "${PROJECT}"-broker; \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml --project-name "${PROJECT}" build ${args}; \
-	else \
-		docker volume create "${PROJECT}-${env}"-db; \
-		docker volume create "${PROJECT}-${env}"-broker; \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${env}".yml --project-name "${PROJECT}-${env}" build ${args}; \
+	@echo $(MESSAGE) "Building stage: ${stage} ${service}"
+	@if [ -z "${stage}" ] && [ -z "${service}" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml build ${args}; \
+	elif [ -z "${stage}" ] && [ -n "${service}" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml build ${service} ${args}; \
+	elif [ -n "${stage}" ] && [ -z "${service}" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml build ${args}; \
+	elif [ -n "${stage}" ] && [ -n "${service}" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml build ${service} ${args}; \
 	fi
 
 docker.down: clean
-	@echo $(MESSAGE) "Down Services Environment: ${env}"
-	@if [ "${env}" == "" ]; then \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml --project-name "${PROJECT}" down --remove-orphans; \
+	@echo $(MESSAGE) "Down Services Stage: ${stage}"
+	@if [ -z  "${stage}" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml down --remove-orphans; \
 	else \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${env}".yml --project-name "${PROJECT}-${env}" down --remove-orphans; \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml down --remove-orphans; \
 	fi
 
 docker.exec: clean
-	@echo $(MESSAGE) "Exec Services Environment: ${env}"
-	@if [ -z "${env}" ]; then \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml --project-name "${PROJECT}" exec ${service} ${command} ; \
+	@echo $(MESSAGE) "Exec Services Stage: ${stage}"
+	@if [ -z "${stage}" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml exec ${service} ${command} ; \
 	else \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${env}".yml --project-name "${PROJECT}-${env}" exec ${service} ${command} ; \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml exec ${service} ${command} ; \
 	fi
 
 docker.stop: clean
-	@echo $(MESSAGE) "Stop Services: ${env}"
-	@if [ "${env}" == "" ]; then \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml --project-name "${PROJECT}" stop; \
+	@echo $(MESSAGE) "Stop Services: ${stage}"
+	@if [ "${stage}" == "" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml stop; \
 	else \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${env}".yml --project-name "${PROJECT}-${env}" stop; \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml stop; \
 	fi
 
 docker.verify_network: ## Verify network
@@ -76,17 +76,17 @@ docker.verify_network: ## Verify network
 	fi
 
 docker.up: clean
-	@echo $(MESSAGE) "Up Services: ${env}"
-	@if [ "${env}" == "" ]; then \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml --project-name "${PROJECT}" up --remove-orphans; \
+	@echo $(MESSAGE) "Up Services: ${stage}"
+	@if [ "${stage}" == "" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml up --remove-orphans; \
 	else \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${env}".yml --project-name "${PROJECT}-${env}" up --remove-orphans; \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml up --remove-orphans; \
 	fi
 
 docker.list: clean
-	@echo $(MESSAGE) "List Services: ${env}"
-	@if [ "${env}" == "" ]; then \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml --project-name "${PROJECT}" ps; \
+	@echo $(MESSAGE) "List Services: ${stage}"
+	@if [ "${stage}" == "" ]; then \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml ps; \
 	else \
-		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${env}".yml --project-name "${PROJECT}-${env}" ps; \
+		$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/"${stage}".yml ps; \
 	fi
