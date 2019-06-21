d00d21ff09db10cd8ea97be836e4a2dc04ff0ead
@slovacus <slovacus@gmail.com>
2019-06-20 22:47:08 -0500

    chore: package origami unnecessary (#223)

--- src/modules/module-appearance.el
+++ src/modules/module-appearance.el
@@ -77,13 +77,6 @@
 (use-package rainbow-mode
   :commands rainbow-mode)
 
-(use-package origami
-  :init
-  (global-origami-mode 1)
-  :config
-  (evil-leader/set-key "fa" 'origami-toggle-all-nodes)
-  (evil-leader/set-key "fr" 'origami-recursively-toggle-node)
-  )
 
 (use-package hlinum
   :config
