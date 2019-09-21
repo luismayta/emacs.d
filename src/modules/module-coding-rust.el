58e26f60a335ad605dd4bfa4189aebddd932821e
@slovacus <slovacus@gmail.com>
2019-09-21 13:46:50 -0500

    (#230) refactor: company racer for rust

--- src/modules/module-coding-rust.el
+++ src/modules/module-coding-rust.el
@@ -5,35 +5,42 @@
   :mode "/\\(Cargo.lock\\|\\.cargo/config\\)\\'")
 
 (use-package rust-mode
+  :hook (rust-mode . lsp))
+
+(use-package rust-mode
   :mode ("\\.rs\\'" . rust-mode)
   :hook electric-pair
   :config
   (setq rust-format-on-save t)
-  (use-package lsp-mode
-    :config
-    (use-package lsp-rust
-      :hook ((rust-mode . lsp-rust-enable)
-	            (rust-mode . flycheck-mode))
-      :config
-      (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls"))))
-  (use-package company-racer
-    :requires company
-    :bind ("M-TAB" . company-complete)
-    :config
-    (setq company-tooltip-align-annotations t))
+  )
 
-  (use-package flycheck-rust
-    :hook ((rust-mode . flycheck-mode)
-	          (flycheck-mode . flycheck-rust-setup)))
+(use-package lsp-rust
+  :disabled
+  :after lsp-mode
+  :config
+  (progn
+    (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls"))
+    (add-hook 'rust-mode-hook #'lsp)
+    ))
 
-  (use-package cargo
-    :hook ((rust-mode . cargo-minor-mode)))
+(use-package company-racer
+  :requires company
+  :bind ("M-TAB" . company-complete)
+  :config
+  (setq company-tooltip-align-annotations t))
 
-  (use-package racer
-    :hook ((racer-mode . eldoc-mode)
-	          (racer-mode . company-mode)
-	          (rust-mode . racer-mode))
-    :bind (("M-." . racer-find-definition))))
+(use-package flycheck-rust
+  :hook ((rust-mode . flycheck-mode)
+	        (flycheck-mode . flycheck-rust-setup)))
+
+(use-package cargo
+  :hook ((rust-mode . cargo-minor-mode)))
+
+(use-package racer
+  :hook ((racer-mode . eldoc-mode)
+	        (racer-mode . company-mode)
+	        (rust-mode . racer-mode))
+  :bind (("M-." . racer-find-definition)))
 
 (provide 'module-coding-rust)
 ;;; module-coding-rust.el ends here
