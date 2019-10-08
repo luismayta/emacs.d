7dff5e3272beae19cc57360fc949bdc775d7bdbf
@slovacus <slovacus@gmail.com>
2019-10-07 20:21:42 -0500

    (#234) chore: action for test

---
+++ provision/make/tests.mk
@@ -0,0 +1,37 @@
+test.help:
+	@echo '    Tests:'
+	@echo ''
+	@echo '        test                      show help'
+	@echo '        test run={module}         Run module test'
+	@echo '        test.all   	             Run all module test'
+	@echo '        test.picked               run test only commit'
+	@echo '        test.lint                 Run all pre-commit'
+	@echo '        test.validate             Run all validation fixture dead in code'
+	@echo ''
+
+test: clean
+	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
+	@if [ -z "${run}" ]; then \
+		make test.help;\
+	fi
+	@if [ -n "${run}" ]; then \
+		$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
+			"$(PIPENV_RUN) py.test ${run}";\
+	fi
+
+test.all: clean
+	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
+	$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
+		"$(PIPENV_RUN) pytest"
+
+test.picked: clean
+	$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
+		"$(PIPENV_RUN) pytest --picked"
+
+test.validate: clean
+	@echo $(MESSAGE) Running tests validation fixture $(END)
+	$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
+		"$(PIPENV_RUN) pytest --dead-fixtures"
+
+test.lint: clean
+	$(PIPENV_RUN) pre-commit run --all-files --verbose
