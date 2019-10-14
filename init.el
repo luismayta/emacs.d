103905f722b909966d9f04996a04f20e66709d61
@slovacus <slovacus@gmail.com>
2019-10-14 01:03:59 -0500

    refactor: load file for functions (#235)

--- init.el
+++ init.el
@@ -1,7 +1,15 @@
 ;;; init.el --- Start of the Emacs initialisation process.
-(setq explicit-shell-file-name "/usr/local/bin/zsh")
+(load-file (concat (file-name-directory load-file-name)
+               "src/core/core-vars.el"))
+(load-file (concat (file-name-directory load-file-name)
+               "src/core/core-load-paths.el"))
+
+(require 'core-vars)
+
+(setq explicit-shell-file-name core-term-shell)
 (setq shell-file-name "zsh")
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 ;;; code:
 (package-initialize)
 (add-to-list 'package-archives
@@ -31,10 +39,6 @@
   (error (concat "Your version of Emacs (%s) is too old. "
            "requires Emacs version %s or above.")
     emacs-version emacs-min-version)
-  (load-file (concat (file-name-directory load-file-name)
-               "src/core/core-vars.el"))
-  (load-file (concat (file-name-directory load-file-name)
-               "src/core/core-load-paths.el"))
   (require 'core-emacs)
   (emacs/init))
 
