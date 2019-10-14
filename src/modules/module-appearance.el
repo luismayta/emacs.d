013d4241b8c355cb3f299bded648ef9654423f36
@slovacus <slovacus@gmail.com>
2019-10-14 00:48:34 -0500

    (#235) chore: clean function install fira code

--- src/modules/module-appearance.el
+++ src/modules/module-appearance.el
@@ -11,7 +11,6 @@
   :ensure t
   )
 
-
 (use-package graphene
   :ensure t
   :init
@@ -122,60 +121,6 @@
 (use-package dired-hacks-utils
   :ensure t)
 
-(def lm-install-fira-code-font
-  (let* ((font-name "FiraCode-Retina.ttf")
-          (font-url
-            (format "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/%s?raw=true" font-name))
-          (font-dest
-            (cl-case window-system
-              (x  (concat (or (getenv "XDG_DATA_HOME")
-                            (concat (getenv "HOME") "/.local/share"))
-                    "/fonts/"))
-              (mac (concat (getenv "HOME") "/Library/Fonts/" ))
-              (ns (concat (getenv "HOME") "/Library/Fonts/" )))))
-    (unless (file-directory-p font-dest) (mkdir font-dest t))
-    (url-copy-file font-url (expand-file-name font-name font-dest) t)
-    (message "Fonts downloaded, updating font cache... <fc-cache -f -v> ")
-    (shell-command-to-string (format "fc-cache -f -v"))
-    (message "Successfully install `fira-code' font to `%s'!" font-dest)))
-
-(def lm-setup-fira-code-font
-  ;; (unless (member "Fira Code" (font-family-list))
-  ;;   (lm-install-fira-code-font))
-  ;; Fira Code enabled
-  ;; https://github.com/tonsky/FiraCode/wiki/Setting-up-Emacs
-  (when (window-system)
-    (set-frame-font "Fira Code Retina-17"))
-  (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
-                  (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
-                  (36 . ".\\(?:>\\)")
-                  (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
-                  (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
-                  (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
-                  (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
-                  (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
-                  (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
-                  (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
-                  (48 . ".\\(?:x[a-zA-Z]\\)")
-                  (58 . ".\\(?:::\\|[:=]\\)")
-                  (59 . ".\\(?:;;\\|;\\)")
-                  (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
-                  (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
-                  (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
-                  (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
-                  (91 . ".\\(?:]\\)")
-                  (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
-                  (94 . ".\\(?:=\\)")
-                  (119 . ".\\(?:ww\\)")
-                  (123 . ".\\(?:-\\)")
-                  (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
-                  (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
-                  )
-          ))
-    (dolist (char-regexp alist)
-      (set-char-table-range composition-function-table (car char-regexp)
-        `([,(cdr char-regexp) 0 font-shape-gstring])))))
-
 (def setup-windows-hook
   (run-with-idle-timer 0.1 nil (lambda nil (toggle-frame-maximized)))
   (when (eq system-type 'darwin)
@@ -183,10 +128,7 @@
     (set-face-attribute 'default nil :height 135))
   (when (eq system-type 'gnu/linux)
     (set-face-attribute 'default nil :height 116))
-  ;; (lm-setup-fira-code-font)
-  ;; (set-frame-font "Fira Code Retina-17"))
   (setq-default truncate-lines 1)  ;; no word wrap
-  ;; (setq-default line-spacing 4)
   )
 
 (add-hook 'emacs-startup-hook #'setup-windows-hook)
