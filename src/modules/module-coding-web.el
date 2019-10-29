276c618e1ecefd765ccced67b2df8c3e4fd566c8
@slovacus <slovacus@gmail.com>
2019-10-29 07:30:45 -0500

    fix: load mode for web (#238)

--- src/modules/module-coding-web.el
+++ src/modules/module-coding-web.el
@@ -2,7 +2,7 @@
 
 (use-package web-mode
   :ensure t
-  :config
+  :init
   (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
@@ -10,7 +10,11 @@
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  :config
+  (setq web-mode-enable-current-element-highlight t))
 
 ;; Enable eslint checker for web-mode
 (flycheck-add-mode 'javascript-eslint 'web-mode)
