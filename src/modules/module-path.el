1defe7a1c3da37db1f267a1847448d4483f16479
@slovacus <slovacus@gmail.com>
2019-10-28 19:55:35 -0500

    style: load vars env for vars (#238)

--- src/modules/module-path.el
+++ src/modules/module-path.el
@@ -2,28 +2,28 @@
 ;; code:
 
 (use-package exec-path-from-shell
-  :defer t ; autoload this package
-  :init
-  (exec-path-from-shell-initialize)
+  :ensure t
+  :if (memq window-system '(mac ns x))
   :config
-  (setq exec-path-from-shell-copy-env "PATH")
-  (setq exec-path-from-shell-copy-env "GOPATH")
-  (setq exec-path-from-shell-copy-env "GOROOT")
-  (setq exec-path-from-shell-copy-env "GO_ENV")
-  (setq exec-path-from-shell-copy-env "PYTHONPATH")
-  (setq exec-path-from-shell-copy-env "DOCKER_HOST")
-  (setq exec-path-from-shell-copy-env "DOCKER_TLS_VERIFY")
-  (setq exec-path-from-shell-copy-env "DOCKER_CERT_PATH")
-  (setq exec-path-from-shell-copy-env "DOCKER_MACHINE_NAME")
-  (setq exec-path-from-shell-copy-env "WAKATIME_API_KEY")
-  (setq exec-path-from-shell-copy-env "WAKATIME_CLI_PATH")
-  (setq exec-path-from-shell-copy-env "WAKATIME_PYTHON_BIN")
-  (setq exec-path-from-shell-check-startup-files nil)
+  (setq exec-path-from-shell-variables '(
+                                          "PATH"
+                                          "GOPATH"
+                                          "GOROOT"
+                                          "GO_ENV"
+                                          "PYTHONPATH"
+                                          "PYENV_DIR"
+                                          "PIPENV_PYTHON"
+                                          "DOCKER_HOST"
+                                          "DOCKER_TLS_VERIFY"
+                                          "DOCKER_CERT_PATH"
+                                          "DOCKER_MACHINE_NAME"
+                                          "WAKATIME_API_KEY"
+                                          "WAKATIME_CLI_PATH"
+                                          "WAKATIME_PYTHON_BIN"
+                                          "LC_ALL"
+                                          ))
   (exec-path-from-shell-initialize))
 
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-(exec-path-from-shell-copy-env "GOPATH")
 
 (provide 'module-path)
 ;;; module-path.el ends here
