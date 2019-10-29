a388aea10269abaffa01843689a1c6ca0cb3a6e1
@slovacus <slovacus@gmail.com>
2019-10-28 19:54:41 -0500

    fix: settings font weight (#238)

--- src/modules/module-fonts.el
+++ src/modules/module-fonts.el
@@ -7,6 +7,10 @@
   (set-default-coding-systems 'utf-8)
   (set-frame-font core/fixed-font-name))
 
+;; Fira code
+;; This works when using emacs --daemon + emacsclient
+(add-hook 'after-make-frame-functions (lambda (frame) (set-fontset-font t '(#Xe100 . #Xe16f) core/fixed-font-name)))
+;; This works when using emacs without server/client
 (set-fontset-font t '(#Xe100 . #Xe16f) core/fixed-font-name)
 
 (use-package
