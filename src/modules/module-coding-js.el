b65784432c0c53ac8c23dd15712284aa6fa386bf
@slovacus <slovacus@gmail.com>
2020-01-19 10:03:53 -0500

    refactor: dependences for coding js (#247)

--- src/modules/module-coding-js.el
+++ src/modules/module-coding-js.el
@@ -2,97 +2,74 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'use-package)
+(use-package add-node-modules-path
+  :hook ((js2-mode . add-node-modules-path)
+          (web-mode . add-node-modules-path)
+          (rjsx-mode . add-node-modules-path)
+          ))
+
+(use-package prettier-js
+  :config
+  (add-hook 'js2-mode-hook 'prettier-js-mode)
+  (add-hook 'web-mode-hook 'prettier-js-mode)
+  (add-hook 'rjsx-mode-hook 'prettier-js-mode)
+  (setq prettier-js-args '(
+                            "--trailing-comma" "all"
+                            "--bracket-spacing" "true"
+                            "--single-quote" "true"
+                            "--no-semi" "true"
+                            "--jsx-single-quote" "true"
+                            "--jsx-bracket-same-line" "true"
+                            "--print-width" "100"))
+  :ensure t  )
 
 (use-package json-mode
   :ensure t
   :mode ("\\.json\\'" ".eslintrc\\'" ".prettierrc\\'" "Pipfile.lock\\'"))
 
 (use-package js2-mode
-  :custom
-  (sgml-basic-offset 2)
-  (sgml-attribute-offset 0)
-  (js-indent-level 2)
-  (js-switch-indent-offset 2)
-  (js2-basic-offset 2)
-  (js2-strict-missing-semi-warning nil)
-  (js2-missing-semi-one-line-override t)
-  (js2-mode-show-parse-errors nil)
-  (js2-mode-show-strict-warnings nil)
-  (js2-mode-strict-inconsistent-return-warning nil)
-  (js2-getprop-has-side-effects t)
-  :init (evil-leader/set-key "gd" 'js2-jump-to-definition)
+  :mode
+  (("\\.js$" . js2-mode)
+    ("\\.jsx$" . js2-jsx-mode))
+  :ensure t
+  :hook ((js2-mode . (lambda ()
+                       (flycheck-mode)
+                       ))
+          (js2-jsx-mode . (lambda ()
+                            (flycheck-mode)
+                            ))
+          (js2-mode . add-node-modules-path)
+          )
   :config
-  (add-to-list 'aggressive-indent-excluded-modes 'js2-mode))
+  ;; have 2 space indentation by default
+  (setq js-indent-level 2
+    js2-basic-offset 2
+    js-chain-indent t))
 
-(use-package js2-refactor
-  :hook ((js2-mode . js2-refactor-mode)
-          (web-mode . js2-refactor-mode))
-  :config
-  (js2r-add-keybindings-with-prefix "C-c C-r"))
+;;; * Use Package indium: javascript awesome development environment
+;;;;   - https://github.com/NicolasPetton/indium
+(use-package indium
+  :ensure t
+  :after js2-mode
+  :bind (:map js2-mode-map
+          ("C-c C-l" . indium-eval-buffer)
+          ("C-c C-n" . indium-run-node)
+          ("C-c b l" . indium-list-breakpoints)
+          ("C-c b K" . indium-remove-all-breakpoints-from-buffer)
+          ("C-c b t" . indium-toggle-breakpoint))
+  :hook ((js2-mode . indium-interaction-mode)))
+
+(use-package js-auto-beautify
+  :ensure t)
+
+(use-package js-import
+  :ensure t)
 
 (use-package rjsx-mode
   :ensure t
   :mode "\\.jsx\\'"
-  :magic ("/\\*\\* @jsx React\\.DOM \\*/" "^import React"))
-
-(use-package tss
-  :mode ("\\.ts\\'" . typescript-mode))
-
-(use-package typescript-mode
-  :mode "\\.tsx?\\'"
-  :custom
-  (typescript-indent-level 2))
-
-(use-package lsp-mode
-  :hook ((js2-mode . lsp)
-          (typescript-mode . lsp))
-  :custom
-  (lsp-prefer-flymake nil))
-
-(use-package company-lsp
-  :config
-  (push 'company-lsp company-backends))
-
-(use-package tide
-  :ensure t
-  :after (typescript-mode company flycheck)
-  :hook ((typescript-mode . tide-setup)
-          (typescript-mode . tide-hl-identifier-mode)
-          (before-save . tide-formater-before-save)))
-
-(use-package emmet-mode
-  :ensure t
-  :hook ((css-mode . emmet-mode)
-          (php-mode . emmet-mode)
-          (sgnl-mode . emmet-mode)
-          (rjsx-mode . emmet-mode)
-          (html-mode . emmet-mode)
-          (web-mode . emmet-mode)))
-
-(use-package js-doc
-  :bind (:map js2-refactor-mode-map
-          ("C-c C-r i d" . js-doc-insert-function-doc)
-          ("@" . js-doc-insert-tag))
-  :custom
-  (js-doc-mail-address "slovacus@gmail.com")
-  (js-doc-author (format "Luis Mayta <%s>" js-doc-mail-address)))
-
-(use-package add-node-modules-path
-  :hook ((js2-mode . add-node-modules-path)
-          (web-mode . add-node-modules-path)))
-
-(use-package prettier-js
-  :config
-  (add-hook 'rjsx-mode-hook 'prettier-js-mode)
-  (add-hook 'js2-mode-hook 'prettier-js-mode)
-  (add-hook 'web-mode-hook 'prettier-js-mode))
-
-;; parse node stack traces in compilation buffers
-(require 'compile)
-(add-to-list 'compilation-error-regexp-alist 'node)
-(add-to-list 'compilation-error-regexp-alist-alist
-  '(node "^[[:blank:]]*at \\(.*(\\|\\)\\(.+?\\):\\([[:digit:]]+\\):\\([[:digit:]]+\\)" 2 3 4))
+  :magic ("/\\*\\* @jsx React\\.DOM \\*/" "^import React")
+  :hook ((rjsx-mode . add-node-modules-path)))
 
 (provide 'module-coding-js)
 ;;; module-coding-js.el ends here
