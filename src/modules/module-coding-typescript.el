91dc8fc33b7bd3e5f6175097d9715614bfa7fb19
@slovacus <slovacus@gmail.com>
2020-02-06 23:32:25 -0500

    refactor: package for js and typescript (#247)

--- src/modules/module-coding-typescript.el
+++ src/modules/module-coding-typescript.el
@@ -1,30 +1,36 @@
 ;; module-coding-typescript.el --- TypeScript.
+(require 'evil)
 
-(use-package typescript-mode
-  :ensure t
-  )
+(use-package web-mode)
 
-(use-package tide
-  :ensure t)
-
-(defun setup-tide-mode ()
+(defun configure-tide ()
   (interactive)
+
   (tide-setup)
   (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
   (company-mode +1))
+
+(use-package tide
+  :commands tide-jump-to-definition
+  :init
+  (evil-define-key 'normal tide-mode-map
+    (kbd "gd") 'tide-jump-to-definition
+    (kbd "gD") 'tide-jump-back
+    )
+  )
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+  (lambda ()
+    (when (string-equal "tsx" (file-name-extension buffer-file-name))
+      (configure-tide))))
 
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
 (setq tide-tsserver-process-environment '("TSS_LOG=-level verbose -file /tmp/tss.log"))
 
 (provide 'module-coding-typescript)
