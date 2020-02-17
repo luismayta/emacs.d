bd5596cdc9f78f9a63c91c0be96adbec46d726fa
@slovacus <slovacus@gmail.com>
2020-02-17 02:38:57 -0500

    feat: load company go and go-mode (#254)

--- src/modules/module-coding-go.el
+++ src/modules/module-coding-go.el
@@ -1,45 +1,54 @@
 ;;; module-coding-go.el --- Golang config.
 ;;; code:
 
+(defvar go-mode)
+(defvar go-eldoc)
+(defvar go-stacktracer)
+(defvar go-autocomplete)
+(defvar before-save-hook)
+
+
 (use-package go-imports)
+
 (setq gofmt-command "goimports")
+
+(use-package go-autocomplete
+  :after (auto-complete-mode)
+  :defer t
+  :config
+  (ac-config-default))
+
+(use-package go-guru
+  :ensure t
+  :config
+  (go-guru-hl-identifier-mode))
+
+(use-package company-go
+  :ensure t
+  :after go
+  :config
+  (setq tab-width 4))
+
 (use-package go-mode
-  :ensure t
+  :after (go-autocomplete)
+  :commands (gofmt-before-save)
+  :defer t
   :config
-  (setq gofmt-command "goimports")
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  :commands
-  (go-mode)
-  :pin melpa)
+  (setq auto-mode-alist
+    (append '(("go.mod" . text-mode)) auto-mode-alist))
+  (add-hook 'go-mode-hook (lambda ()
+                            (setq indent-tabs-mode t)
+                            (setq tab-width 4)))
+  (add-hook 'before-save-hook #'gofmt-before-save))
 
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
-
-(use-package go-errcheck)
-
-(use-package golint)
-
-;; go-add-tags
-(use-package go-add-tags
-  :ensure t
-  :config
-  (with-eval-after-load 'go-mode
-    (define-key go-mode-map (kbd "C-c t") #'go-add-tags)))
-
-;; go-eldoc
 (use-package go-eldoc
-  :ensure t
+  :after (go-mode)
+  :commands (go-eldoc-setup)
+  :diminish eldoc-mode
   :config
-  (add-hook 'go-mode-hook 'go-eldoc-setup))
+  (add-hook 'go-mode-hook #'go-eldoc-setup))
 
-(use-package speedbar
-  :config
-  (speedbar-add-supported-extension ".go"))
-
-(use-package go-complete
-  :init
-  (add-hook 'completion-at-point-functions 'go-complete-at-point))
+(use-package go-stacktracer)
 
 (provide 'module-coding-go)
 ;;; module-coding-go.el ends here
