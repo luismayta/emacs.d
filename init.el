2c7a0b88663146729c359f9b1a7569ef07492bfe
@slovacus <slovacus@gmail.com>
2020-03-05 05:32:58 -0500

    chore: implement pakage marmalade (#258)

--- init.el
+++ init.el
@@ -18,6 +18,8 @@
 (add-to-list 'package-archives
   '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
   '("melpa" . "https://melpa.org/packages/") t )
 
 
