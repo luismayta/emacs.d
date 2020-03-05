c5b842496f4c89b70680ccf1e6a73d16edce5a3d
@slovacus <slovacus@gmail.com>
2020-03-05 06:41:20 -0500

    refactor: dependence for http-restclient (#258)

--- src/modules/module-tags.el
+++ src/modules/module-tags.el
@@ -2,24 +2,31 @@
 
 ;;; code:
 
-;; (use-package counsel-gtags
-;;   :diminish counsel-gtags-mode
-;;   :init
-;;   (evil-leader/set-key "tt" 'counsel-gtags-find-tag)
-;;   (evil-leader/set-key "tc" 'counsel-gtags-update-tags)
-;;   (add-hook 'prog-mode-hook 'counsel-gtags-mode)
-;;   (add-hook 'before-save-hook 'counsel-gtags-update-tags)
-;;   )
+(use-package counsel-gtags
+  :diminish counsel-gtags-mode
+  :init
+  (evil-leader/set-key "tt" 'counsel-gtags-find-tag)
+  (evil-leader/set-key "tc" 'counsel-gtags-update-tags)
+  (add-hook 'prog-mode-hook 'counsel-gtags-mode)
+  (add-hook 'before-save-hook 'counsel-gtags-update-tags)
+  )
 
-;; (use-package ggtags
-;;   :defer t
-;;   :init
-;;   (add-hook 'prog-mode-hook 'ggtags-mode)
-;;   (add-hook 'awk-mode-local-vars-hook #'ggtags-mode-enable)
-;;   (add-hook 'shell-mode-local-vars-hook #'ggtags-mode-enable)
-;;   (add-hook 'tcl-mode-local-vars-hook #'ggtags-mode-enable)
-;;   (add-hook 'vhdl-mode-local-vars-hook #'ggtags-mode-enable)
-;;   )
+(use-package ggtags
+  :ensure t
+  :defer
+  :diminish
+  ggtags-mode
+  :init
+  (evil-leader/set-key
+    "ft" 'ggtags-find-tag-dwim
+    "fr" 'ggtags-find-reference
+    "fd" 'ggtags-find-definition
+    "fc" 'ggtags-find-tag-continue)
+  :commands
+  ggtags-find-tag-dwim
+  ggtags-find-reference
+  ggtags-find-definition
+  ggtags-find-tag-continue)
 
 (provide 'module-tags)
 ;;; module-tags.el ends here
