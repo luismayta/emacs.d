b3ac8ae2cfe4a69e093c177683bd423bac6efbf7
@slovacus <slovacus@gmail.com>
2020-04-01 03:31:23 -0500

    refactor: typescript mode with tide (#261)

--- src/modules/module-coding-typescript.el
+++ src/modules/module-coding-typescript.el
@@ -1,29 +1,33 @@
 ;; module-coding-typescript.el --- TypeScript.
 (require 'evil)
 
-(use-package web-mode)
-
-(defun configure-tide ()
+(defun my/setup-tide-mode ()
+  "Set up tide mode."
   (interactive)
-
+  (setq company-tooltip-align-annotations t)
   (tide-setup)
-  (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
-  (company-mode +1))
+  (company-mode +1)
+  )
+
+(use-package typescript-mode)
 
 (use-package tide
-  :requires (company eldoc flycheck web-mode)
-  :commands tide-jump-to-definition
-  )
+  :config
+  :config
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'typescript-mode-hook 'my/setup-tide-mode)
+  (add-to-list 'aggressive-indent-excluded-modes 'typescript-mode)
+  (add-hook 'js2-mode-hook 'my/setup-tide-mode))
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
 (add-hook 'web-mode-hook
   (lambda ()
     (when (string-equal "tsx" (file-name-extension buffer-file-name))
-      (configure-tide))))
+      (my/setup-tide-mode))))
 
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
--- src/modules/module-coding-web.el
+++ src/modules/module-coding-web.el
@@ -11,7 +11,7 @@
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts[x]\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
   :config
   (setq web-mode-enable-current-element-highlight t))
