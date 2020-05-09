87f8416fc0141908b4fad442544ce69b7838cc46
@slovacus <slovacus@gmail.com>
2020-05-09 03:32:13 -0500

    chore: implement actions for keybase (#267)

--- provision/make/utils.mk
+++ provision/make/utils.mk
@@ -14,10 +14,14 @@ utils:
 	make utils.help
 
 utils.generate:
+	mkdir -p ${KEYBASE_PROJECT_PATH}/${stage}/{pem,pub,private,openssl}
+	mkdir -p ${KEYBASE_PROJECT_PATH}/${stage}/pub
+	mkdir -p ${KEYBASE_PROJECT_PATH}/${stage}/private
+	mkdir -p ${KEYBASE_PROJECT_PATH}/${stage}/openssl
+
 	ssh-keygen -q -m PEM -t rsa -b 4096 -C "admin@${PROJECT}-${stage}.com" -f ${PROJECT}-${stage} -P ""
 	openssl rsa -in ${PROJECT}-${stage} -outform pem > ${PROJECT}-${stage}.pem
 	chmod 0600 ${PROJECT}-${stage}.pem
-	cp -rf ${PROJECT}-${stage}.pem ${KEYS_PEM_DIR}
-	cp -rf ${PROJECT}-${stage}.pub ${KEYS_PUB_DIR}
-	cp -rf ${PROJECT}-${stage}.pub ${KEYS_PRIVATE_DIR}
-	cp -rf ${PROJECT}-${stage} ${KEYS_PRIVATE_DIR}
+	cp -rf ${PROJECT}-${stage}.pem ${KEYBASE_PROJECT_PATH}/${stage}/pem/
+	cp -rf ${PROJECT}-${stage}.pub ${KEYBASE_PROJECT_PATH}/${stage}/pub/
+	cp -rf ${PROJECT}-${stage} ${KEYBASE_PROJECT_PATH}/${stage}/private/
