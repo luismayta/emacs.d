96ee1e7df612539231b1d90c0dcd8b093c8c5de1
@slovacus <slovacus@gmail.com>
2020-05-09 10:55:43 -0500

    refactor: settings for prettier (#269)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-const prettierrc = require('@equipindustry/prettierrc');
 module.exports = {
   extends: ['@equipindustry/eslint-config'],
-  plugins: ['prettier', 'react-hooks', 'jsx-a11y'],
   settings: {},
   env: {
     browser: true,
@@ -15,36 +13,4 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  rules: {
-    'prettier/prettier': [
-      'error',
-      prettierrc,
-      {
-        usePrettierrc: true,
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      env: { browser: true, es6: true, node: true },
-      extends: ['@equipindustry/eslint-config'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-      },
-      plugins: ['prettier', '@typescript-eslint'],
-      rules: {
-        'prettier/prettier': [
-          'error',
-          prettierrc,
-          {
-            usePrettierrc: true,
-          },
-        ],
-      },
-    },
-  ],
 };
