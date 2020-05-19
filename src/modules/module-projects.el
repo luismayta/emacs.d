4dda5a692c82d5aadfecdc78be4fcdf9ca371e73
@slovacus <slovacus@gmail.com>
2020-05-19 09:08:00 -0500

    fix: settings for load command project-find-file (#274)

--- src/modules/module-projects.el
+++ src/modules/module-projects.el
@@ -6,14 +6,37 @@
 (use-package projectile
   :bind-keymap
   ("C-c q" . projectile-command-map)
+  :hook (after-init . projectile-mode)
   :init
   (setq projectile-keymap-prefix (kbd "C-c q"))
+  (setq projectile-mode-line-prefix ""
+        projectile-sort-order 'recentf
+        projectile-use-git-grep t)
   :config
   (setq projectile-completion-system 'ivy)
-  (setq projectile-require-project-root nil)
-  (projectile-mode)
-  :delight
-  projectile-mode 1)
+  (cond
+   ((executable-find "fd")
+    (setq projectile-generic-command
+          (format "%s . --color=never --type f -0 -H -E .git"
+                  "fd")
+          projectile-git-command projectile-generic-command
+          projectile-git-submodule-command nil
+          ;; ensure Windows users get fd's benefits
+          projectile-indexing-method 'alien))
+
+   ((executable-find "rg")
+    (setq projectile-generic-command
+          (concat "rg -0 --files --color=never --hidden"
+                  (cl-loop for dir in projectile-globally-ignored-directories
+                           concat (format " --glob '!%s'" dir)))
+          projectile-git-command projectile-generic-command
+          projectile-git-submodule-command nil
+          ;; ensure Windows users get rg's benefits
+          projectile-indexing-method 'alien)))
+
+  (when IS-WINDOWS
+    (setq projectile-git-submodule-command nil
+          projectile-enable-caching nil)))
 
 
 (use-package counsel-projectile
