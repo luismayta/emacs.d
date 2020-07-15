5ad91c3b3ca351a8a526bd787e06d50e43b81dec
@slovacus <slovacus@gmail.com>
2020-07-15 12:41:44 -0500

    refactor: settings for project (#283)

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,56 @@
-module.exports = require('@equipindustry/prettierrc');
+module.exports = {
+  tabWidth: 2,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  semi: false,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  singleQuote: true,
+  overrides: [
+    {
+      files: '{*.js?(on),*.y?(a)ml,.*.js?(on),.*.y?(a)ml,*.md,.prettierrc,.stylelintrc,.babelrc}',
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: '{**/.vscode/*.json,**/tsconfig.json,**/tsconfig.*.json,.stylelintrc}',
+      options: {
+        parser: 'json',
+        quoteProps: 'preserve',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown',
+        printWidth: 100,
+        proseWrap: 'never',
+        semi: false,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: '*.mdx',
+      options: {
+        printWidth: 100,
+        proseWrap: 'never',
+        semi: false,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: '*.{sass,scss}',
+      options: {
+        parser: 'scss',
+      },
+    },
+    {
+      files: '*.{gql,graphql}',
+      options: {
+        parser: 'graphql',
+      },
+    },
+  ],
+}
