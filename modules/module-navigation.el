1c804da67e2f5b50f92346fcdf8fe6ddbad8f8ec
@slovacus <slovacus@gmail.com>
2020-12-07 09:56:06 -0500

    refactor: install package neotree delete treemacs (#300)

--- modules/module-navigation.el
+++ modules/module-navigation.el
@@ -14,6 +14,31 @@
   ("C-x l". windmove-right)
   )
 
+;; neotree
+(use-package neotree
+  :ensure t
+  :requires (evil-leader)
+  :after projectile
+  :init
+  (setq neo-smart-open t)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (evil-leader/set-key "n"  'neotree-toggle)
+  (setq projectile-switch-project-action 'neotree-projectile-action)
+  :config
+  (evil-define-key 'normal neotree-mode-map (kbd "+") 'neotree-create-node)
+  (evil-define-key 'normal neotree-mode-map (kbd "R") 'neotree-rename-node)
+  (evil-define-key 'normal neotree-mode-map (kbd "D") 'neotree-delete-node)
+  (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+  (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+  (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+  (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "U") 'neotree-select-up-node)
+  (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle))
+
 ;; saveplace
 ;; Remebers your location in a file when saving files.
 (use-package saveplace
