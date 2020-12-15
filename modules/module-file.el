bf99333d8b3395380db3faab31b9edeab174f42f
@slovacus <slovacus@gmail.com>
2020-12-14 23:44:26 -0500

    chore: implement package vimrc mode (#303)

---
+++ modules/module-file.el
@@ -0,0 +1,12 @@
+;;; module-file.el --- Golang config.
+;;; code:
+
+(use-package vimrc-mode
+  :mode (("/\\.vimrc\\'" . vimrc-mode)
+          ("/vimrc\\'" . vimrc-mode)
+          ("\\.vim\\'" . vimrc-mode)))
+
+(use-package syslog-mode :mode "/var/log.*\\'")
+
+(provide 'module-file)
+;;; module-file.el ends here
