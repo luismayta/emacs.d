9cb2860da527856630723f9c207364d4b5d58d75
@slovacus <slovacus@gmail.com>
2021-01-03 01:50:21 -0500

    refactor: load packages performance (#305)

--- modules/module-completion.el
+++ modules/module-completion.el
@@ -17,38 +17,6 @@
 
 ;;; Code:
 
-
-;; Ivy (taken from "How to make your own Spacemacs")
-(use-package ivy
-  :ensure t
-  :diminish (ivy-mode . "") ; does not display ivy in the modeline
-  :init (ivy-mode 1)        ; enable ivy globally at startup
-  :config
-  (setq ivy-use-virtual-buffers t)   ; extend searching to bookmarks and …
-  (setq ivy-height 10)               ; set height of the ivy window
-  (setq ivy-count-format "(%d/%d) ") ; count format, from the ivy help page
-  )
-
-;; Counsel (same as Ivy above)
-(use-package counsel
-  :ensure t
-  :requires (evil-leader)
-  :init
-  (evil-leader/set-key "/" 'counsel-grep)
-  :commands          ; Load counsel when any of these commands are invoked
-  (counsel-M-x       ; M-x use counsel
-    counsel-find-file ; C-x C-f use counsel-find-file
-    counsel-recentf   ; search recently edited files
-    counsel-git       ; search for files in git repo
-    counsel-git-grep  ; search for regexp in git repo
-    counsel-ag        ; search for regexp in git repo using ag
-    counsel-locate)   ; search for files or else using locate
-  )
-
-;; optional package to get the error squiggles as you edit
-(use-package flycheck
-  :ensure t)
-
 (use-package company
   :defer 5
   :diminish
---
+++ modules/module-core.el
@@ -0,0 +1,36 @@
+;;; module-core.el --- Core settings.
+;;; code:
+
+;; eliminate scroll bars and do other things
+(use-package better-defaults
+  :ensure t
+  )
+
+(use-package exec-path-from-shell
+  :ensure t
+  :if (memq window-system '(mac ns x))
+  :config
+  (setq exec-path-from-shell-variables '(
+                                          "PATH"
+                                          "GOPATH"
+                                          "GOROOT"
+                                          "GO_ENV"
+                                          "GO111MODULES"
+                                          "PYTHONPATH"
+                                          "PYENV_DIR"
+                                          "PIPENV_PYTHON"
+                                          "DOCKER_HOST"
+                                          "DOCKER_TLS_VERIFY"
+                                          "DOCKER_CERT_PATH"
+                                          "DOCKER_MACHINE_NAME"
+                                          "LC_ALL"
+                                          ))
+  (exec-path-from-shell-initialize)
+  )
+
+(use-package format-all
+)
+
+
+(provide 'module-core)
+;;; module-core.el ends here
