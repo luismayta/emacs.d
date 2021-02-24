44342f4fa6a41c32d4f7f12cd8319030694ca123
@slovacus <slovacus@gmail.com>
2021-02-24 11:08:01 -0500

    chore: implement config user

--- init.el
+++ init.el
@@ -1,22 +1,285 @@
-;;; init.el --- Start of the Emacs initialisation process.
 
-;;; code:
+;;; --- Bootstrap
+
+;; elpa package management
+(require 'package)
+
+;; common lispy things
+(require 'cl)
+
+;; melpa (github-based) source
+(add-to-list 'package-archives
+  '("melpa" .
+   "http://melpa.milkbox.net/packages/"))
+
+;; marmalade source
+(add-to-list 'package-archives
+  '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+
+;;;el get install
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (require 'package)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/"))
+  (package-refresh-contents)
+  (package-initialize)
+  (package-install 'el-get)
+  (require 'el-get))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+;; start the package system
 (package-initialize)
 
-;; Increase the GC threshold as soon as possible.
-(setq gc-cons-threshold 50000000)
+;; Specify a dependency (auto-install)
+(defun dep (depname)
+  "Require or install a dependency as needed."
+  (interactive)
+  (unless (package-installed-p depname)
+    (install-dep depname nil)))
 
-(defconst project-version         "1.4.0" "Version.")
-(defconst emacs-min-version   "24.4" "Minimal version of Emacs.")
+;; Convenience around `package-install'.
+(defun install-dep (depname refresh)
+  "Runs `package-install', attempting `package-refresh-contents' on failure."
+  (when refresh (package-refresh-contents))
+  (condition-case err
+      (package-install depname)
+    (error (if refresh
+               (signal (car err) (cdr err))
+             (install-dep depname t)))))
 
-(if (not (version<= emacs-min-version emacs-version))
-  (error (concat "Your version of Emacs (%s) is too old. "
-           "requires Emacs version %s or above.")
-    emacs-version emacs-min-version)
-  (load-file (concat (file-name-directory load-file-name)
-               "core/core-load-paths.el"))
-  (require 'core-emacs)
-  (emacs/init))
+;; Specify a list of dependencies
+(defun dependencies (deps)
+  "Convenience around `dep' to load multiple deps."
+  (unless (eq '() deps)
+    (dep (car deps))
+    (dependencies (cdr deps))))
 
-(provide 'init)
-;;; init.el ends here
+;; Macro to return an interactive lambda.
+(defmacro run (cmd)
+  `(lambda () (interactive) ,cmd))
+
+;;; -- Dependencies
+
+(dependencies '(subatomic256-theme
+                evil
+                evil-numbers
+                paredit
+                erlang
+                auto-complete
+                markdown-mode
+                yaml-mode
+                web-mode
+                slime
+                fiplr
+                php-mode))
+
+;; Do what I mean for the TAB key.
+(defun dwim-tab ()
+  "In most cases, performs the default action for the TAB key.
+If pressing TAB would do nothing, indents by one tab stop."
+  (interactive)
+  (when (or (eq last-command this-command)
+            (= (point) (progn
+                         (indent-for-tab-command)
+                         (point))))
+    (tab-to-tab-stop)))
+
+;; dwim-tab-mode for logical tab key behaviour
+(define-minor-mode dwim-tab-mode
+  "Toggle dwim-tab-mode on/off.
+Given a non-nil argument, enables dwim-tab.
+With dwim-tab-mode enabled, pressing TAB multiple times continues to indent."
+  :lighter " DWIM"
+  :global  t
+  :keymap  `((,(kbd "TAB") . dwim-tab)))
+
+;; no fucking latin-1, thank you very much
+(mapc (lambda (fn) (funcall fn 'utf-8))
+      '(set-terminal-coding-system
+        set-keyboard-coding-system
+        prefer-coding-system))
+
+;;; -- Config
+
+;; make pretty colors
+(load-theme 'noctilux t)
+
+;; emacs is actually vim in disguise
+(evil-mode t)
+
+;; show the column number in the status bar
+(column-number-mode t)
+
+;; turn on ido mode everywhere
+(ido-mode t)
+
+;; show matching braces
+(show-paren-mode t)
+
+;; the menu bar is pointless in a terminal
+(menu-bar-mode -1)
+
+;; don't show the tool bar when in a gui
+(when (featurep 'tool-bar)
+  (tool-bar-mode -1))
+
+;; turn on auto-completion of function names etc
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+;; global key mappings
+(mapc (lambda (mapping)
+        (global-set-key (kbd (car mapping)) (cdr mapping)))
+      `(;; toggle line numbers
+        ("C-x j"   . ,(run (linum-mode (or (not linum-mode) 0))))
+        ;; open ~/.emacs.d/init.el
+        ("C-x /"   . ,(run (find-file user-init-file)))
+        ;; run an emacs command
+        ("C-x SPC" . execute-extended-command)
+        ;; run a lisp expression
+        ("C-x ,"   . eval-expression)
+        ;; find file in project
+        ("C-x f"   . fiplr-find-file)))
+
+;; evil normal mode key mappings
+(mapc (lambda (mapping)
+        (define-key evil-normal-state-map (kbd (car mapping)) (cdr mapping)))
+      `(;; increment number under point
+        ("C-k"   . evil-numbers/inc-at-pt)
+        ;; decrement number under point
+        ("C-j"   . evil-numbers/dec-at-pt)))
+
+;; evil leader 
+(global-evil-leader-mode)
+
+;;set evil leader
+(evil-leader/set-leader ",")
+
+;; mapping keys evil leader
+(evil-leader/set-key
+  "e" 'find-file
+  "n" 'neotree-toggle
+  "f" 'fiplr-find-file
+  "b" 'switch-to-buffer
+  "k" 'kill-buffer)
+
+;; allow the arrow keys to be used for cycling windows
+(mapc (lambda (keys)
+        (let ((letter (format "C-w %s" (car keys)))
+              (arrow  (format "C-w %s" (cdr keys))))
+          (define-key evil-normal-state-map (kbd arrow) (kbd letter))
+          (define-key evil-motion-state-map (kbd arrow) (kbd letter))
+          (define-key evil-visual-state-map (kbd arrow) (kbd letter))))
+      '(("h" . "<left>")
+        ("j" . "<down>")
+        ("k" . "<up>")
+        ("l" . "<right>")))
+
+;; show whitespace...
+(global-whitespace-mode t)
+
+;; reload changes from disk
+(global-auto-revert-mode t)
+
+;; customize some global vars
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-auto-start nil)
+ '(ac-trigger-key "TAB")
+ '(auto-save-default nil)
+ '(custom-safe-themes
+   (quote
+    ("a99e7c91236b2aba4cd374080c73f390c55173c5a1b4ac662eeb3172b60a9814" "64581032564feda2b5f2cf389018b4b9906d98293d84d84142d90d7986032d33" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" default)))
+ '(evil-shift-width 2)
+ '(evil-toggle-key (kbd "C-\\"))
+ '(ido-enable-flex-matching t)
+ '(indent-tabs-mode nil)
+ '(make-backup-files nil)
+ '(require-final-newline t)
+ '(ruby-deep-indent-paren nil)
+ '(ruby-insert-encoding-magic-comment nil)
+ '(scroll-conservatively 1000)
+ '(scroll-step 1)
+ '(tab-stop-list (number-sequence 2 200 2))
+ '(tab-width 4)
+ '(global-linum-mode t)
+ '(column-number-mode t)
+ '(whitespace-style (quote (face trailing))))
+
+;; treat underscores as word chars
+(modify-syntax-entry ?_ "w")
+
+;; .md files should use markdown-mode
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; templates web mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jade\\'" . web-mode))
+
+;; complete parenthesis
+(setq skeleton-pair t)
+(global-set-key "[" 'skeleton-pair-insert-maybe)
+(global-set-key "(" 'skeleton-pair-insert-maybe)
+(global-set-key "{" 'skeleton-pair-insert-maybe)
+(global-set-key "'" 'skeleton-pair-insert-maybe)
+(global-set-key "\"" 'skeleton-pair-insert-maybe)
+
+;; make php-mode indentation vaguely sane
+(add-hook 'php-mode-hook
+  (lambda ()
+    (setq indent-tabs-mode t)
+    (setq tab-width 4)
+    (setq evil-shift-width 4)
+    (set (make-local-variable 'tab-stop-list) (number-sequence 4 200 4))
+    (c-set-offset 'substatement-open 0)))
+
+;; make jedi-mode 
+(autoload 'jedi:setup "jedi" nil t)
+(setq jedi:setup-keys t)
+(add-hook 'python-mode-hook 'jedi:setup)
+
+;; magic to change the mode-line color according to state
+(lexical-let ((default-color (cons (face-background 'mode-line)
+                                   (face-foreground 'mode-line))))
+  (add-hook 'post-command-hook
+    (lambda ()
+      (let ((color (cond ((minibufferp) default-color)
+                         ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+                         ((evil-emacs-state-p)  '("#af00d7" . "#ffffff"))
+                         ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                         (t default-color))))
+        (set-face-background 'mode-line (car color))
+        (set-face-foreground 'mode-line (cdr color))))))
+
+
+;;; -- User config
+
+(let ((custom-config "~/.emacs.d/custom.el"))
+  (when (file-exists-p custom-config)
+    (load custom-config)))
+
+;; Don't move back the cursor one position when exiting insert mode' 
+(setq evil-move-cursor-back nil)
+
+;;; .emacs ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
