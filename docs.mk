75d327929694adf8c7dc73d0e7dda33227f537f6
@slovacus <slovacus@gmail.com>
2021-02-24 12:41:53 -0500

    chore: implement actions for makefile 1.5.2

--- docs.mk
+++ docs.mk
@@ -2,12 +2,18 @@
 # See ./CONTRIBUTING.rst
 #
 
+FILE_README=$(ROOT_DIR)/README.rst
+PATH_DOCKER_COMPOSE:=provision/docker-compose
+
+docs.help:
+	@echo '    Docs:'
+	@echo ''
+	@echo '        docs.show                  Show restview README'
+	@echo '        docs.make.html             Make documentation html'
+	@echo ''
+
 docs.show: clean
-	@make clean
 	restview "${FILE_README}"
 
 docs.make.html: clean
-	docker-compose run --rm docs bash -c "cd docs && make html"
-
-docs.make.pdf: clean
-	docker-compose run --rm docs bash -c "cd docs && make latexpdf"
+	$(docker-compose) -f "${PATH_DOCKER_COMPOSE}"/dev.yml run --rm docs bash -c "cd docs && make html"
