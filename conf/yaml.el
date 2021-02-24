1704b019ef571a28e33b3de1b9f6cc29abc7ec06
@slovacus <slovacus@gmail.com>
2021-02-24 16:57:18 -0500

    build: clean unnecessary conf (#309)

--- conf/yaml.el
+++
@@ -1,9 +0,0 @@
-;; Install Packages
-(bundle 'yaml-mode nil)
-
-(require 'yaml-mode)
-
-(defun yaml-mode-hook ()
-  (autoload 'yaml-mode "yaml-mode" nil t)
-  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-  (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode)))
