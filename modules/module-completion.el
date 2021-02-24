f8ea96115180049ec204a8e8fabe1f3cefeb05d5
@slovacus <slovacus@gmail.com>
2021-02-24 18:42:27 -0500

    refactor: company and company package (#311)

--- modules/module-completion.el
+++ modules/module-completion.el
@@ -16,52 +16,45 @@
 ;;   - company
 
 ;;; Code:
-
 (use-package company
-  :defer 5
-  :diminish
-  :init
-  (setq company-idle-delay 0.2)
-  (setq company-minimum-prefix-length 2)
-  (setq company-show-numbers t)
-  (setq company-tooltip-limit 20)
-
-  (setq company-dabbrev-downcase nil)
-  (setq company-dabbrev-ignore-case t)
-
-  (setq company-dabbrev-code-ignore-case t)
-  (setq company-dabbrev-code-everywhere t)
-
-  (setq company-etags-ignore-case t)
-
-  (setq company-global-modes
-    '(not
-       eshell-mode comint-mode org-mode erc-mode))
-  :config
-  (add-to-list 'company-backends 'company-capf)
-
-  (defadvice company-complete-common (around advice-for-company-complete-common activate)
-    (when (null (yas-expand))
-      ad-do-it))
-
-  (defun my-company-tab ()
-    (interactive)
-    (when (null (yas-expand))
-      (company-select-next)))
-
-  (add-hook 'after-init-hook 'global-company-mode))
+	:custom
+		(
+			(company-idle-delay 1)
+			(company-echo-delay 0)
+      (company-show-numbers t)
+      (company-tooltip-limit 20)
+			(company-selection-wrap-around t)
+			(company-minimum-prefix-length 3)
+			(company-tooltip-align-annotations t)
+			(company-dabbrev-downcase nil)
+			;(company-frontends '(company-box-frontend))
+		)
+	:bind
+	(
+		:map company-active-map
+			("RET" . nil)
+			("TAB" . company-complete-selection)
+			("<tab>" . company-complete-selection)
+			("C-s" . company-complete-selection)  ; Mostly to use during yasnippet expansion
+			("C-n" . company-select-next)
+			("C-p" . company-select-previous)
+	)
+	:config
+		(global-company-mode)
+)
 
 (use-package company-box
-  :requires (lsp-mode)
-  :diminish
-  :after (company)
-  :hook (company-mode . company-box-mode))
-:config
-(setq company-box-backends-colors nil
-  company-box-show-single-candidate t
-  company-box-max-candidates 50
-  company-box-doc-delay 0.5)
-
+	:after (company)
+	:custom
+	(
+		(company-box-show-single-candidate t)
+    (company-box-backends-colors nil) ;; Same colors for all backends
+	)
+	:custom-face
+		(company-box-selection ((t (:inherit company-tooltip-selection :extend t))))
+	:hook
+		(company-mode . company-box-mode)
+)
 
 (provide 'module-completion)
 ;;; module-completion.el ends here
