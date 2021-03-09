5d1bff36869f0b192e0e8c60eeab720ffb0fc743
@slovacus <slovacus@gmail.com>
2021-03-09 13:27:36 -0500

    feat: implement package quelpa to use-package (#318)

--- init.el
+++ init.el
@@ -12,17 +12,14 @@
 (require 'config-default)
 (require 'config-backup)
 
-;; enable melpa if it isn't enabled
-(require 'package)
+(setq package-archives '(
+                         ("gnu"   . "https://elpa.gnu.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         )
+      )
 
-(add-to-list 'package-archives
-  '("gnu" . "http://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives
-  '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives
- '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-  '("melpa" . "https://melpa.org/packages/") t )
+(package-initialize)
 
 (unless package-archive-contents
   (package-refresh-contents)
@@ -31,6 +28,13 @@
     (package-install 'use-package)
     (setq use-package-always-ensure t))
   )
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package quelpa-use-package
+  :custom
+  (quelpa-checkout-melpa-p nil))
 
 (defconst project-version     "1.6.3" "Version.")
 (defconst emacs-min-version   "24.4" "Minimal version of Emacs.")
