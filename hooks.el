f29fdd03a73dce2cc65b2865ad0881278a5d3729
@slovacus <slovacus@gmail.com>
2021-03-09 13:29:14 -0500

    build: clean unnecessary packages (#318)

--- hooks.el
+++
@@ -1,8 +0,0 @@
-;; Hooks Functions
-(provide 'functions.el)
-(require 'whitespace)
-
-(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
-
-(add-hook 'before-save-hook 'hook-save-buffer)
-;; (add-hook 'before-save-hook 'whitespace-cleanup)
