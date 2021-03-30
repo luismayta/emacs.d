0463e97a4682167ef6f86ce8095a3b7a415e3e8c
@slovacus <slovacus@gmail.com>
2021-03-29 22:04:20 -0500

    chore: implement lsp for ruby (#320)

--- modules/module-completion.el
+++ modules/module-completion.el
@@ -18,13 +18,15 @@
 ;;; Code:
 (use-package company
   :quelpa (company :fetcher github
-		   :repo "company-mode/company-mode")
+		        :repo "company-mode/company-mode")
   :diminish company-mode
   :config
   (setq company-tooltip-align-annotations t)
+  (setq company-minimum-prefix-length 1
+    company-idle-delay 0.0) ;; default is 0.2
   :init
   (global-company-mode t)
-)
+  )
 
 (provide 'module-completion)
 ;;; module-completion.el ends here
