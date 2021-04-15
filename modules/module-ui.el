aa04f1485c0dd146ad01f73f42524a13bb9f7f3f
@slovacus <slovacus@gmail.com>
2021-04-14 21:18:42 -0500

    chore: implement company-box (#322)

--- modules/module-ui.el
+++ modules/module-ui.el
@@ -56,18 +56,65 @@
     )
   )
 
+(use-package company-box
+  :diminish company-box-mode
+  :hook (company-mode . company-box-mode)
+  :init
+  (setq company-box-icons-alist 'company-box-icons-all-the-icons)
+  :config
+  (require 'all-the-icons)
+  (setf (alist-get 'min-height company-box-frame-parameters) 6)
+  (setq company-box-icons-alist 'company-box-icons-all-the-icons
+    company-box-backends-colors nil
+
+    ;; These are the Doom Emacs defaults
+    company-box-icons-all-the-icons
+    `((Unknown       . ,(all-the-icons-material "find_in_page"             :face 'all-the-icons-purple))
+       (Text          . ,(all-the-icons-material "text_fields"              :face 'all-the-icons-green))
+       (Method        . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+       (Function      . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+       (Constructor   . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+       (Field         . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+       (Variable      . ,(all-the-icons-material "adjust"                   :face 'all-the-icons-blue))
+       (Class         . ,(all-the-icons-material "class"                    :face 'all-the-icons-red))
+       (Interface     . ,(all-the-icons-material "settings_input_component" :face 'all-the-icons-red))
+       (Module        . ,(all-the-icons-material "view_module"              :face 'all-the-icons-red))
+       (Property      . ,(all-the-icons-material "settings"                 :face 'all-the-icons-red))
+       (Unit          . ,(all-the-icons-material "straighten"               :face 'all-the-icons-red))
+       (Value         . ,(all-the-icons-material "filter_1"                 :face 'all-the-icons-red))
+       (Enum          . ,(all-the-icons-material "plus_one"                 :face 'all-the-icons-red))
+       (Keyword       . ,(all-the-icons-material "filter_center_focus"      :face 'all-the-icons-red))
+       (Snippet       . ,(all-the-icons-material "short_text"               :face 'all-the-icons-red))
+       (Color         . ,(all-the-icons-material "color_lens"               :face 'all-the-icons-red))
+       (File          . ,(all-the-icons-material "insert_drive_file"        :face 'all-the-icons-red))
+       (Reference     . ,(all-the-icons-material "collections_bookmark"     :face 'all-the-icons-red))
+       (Folder        . ,(all-the-icons-material "folder"                   :face 'all-the-icons-red))
+       (EnumMember    . ,(all-the-icons-material "people"                   :face 'all-the-icons-red))
+       (Constant      . ,(all-the-icons-material "pause_circle_filled"      :face 'all-the-icons-red))
+       (Struct        . ,(all-the-icons-material "streetview"               :face 'all-the-icons-red))
+       (Event         . ,(all-the-icons-material "event"                    :face 'all-the-icons-red))
+       (Operator      . ,(all-the-icons-material "control_point"            :face 'all-the-icons-red))
+       (TypeParameter . ,(all-the-icons-material "class"                    :face 'all-the-icons-red))
+       (Template      . ,(all-the-icons-material "short_text"               :face 'all-the-icons-green))))
+
+  ;; Add a space after the icon
+  (dolist (elt company-box-icons-all-the-icons)
+    (setcdr elt (concat (cdr elt) " "))))
+
 (use-package doom-modeline
   :ensure t
   :defer t
   :init (doom-modeline-init)
   :config
   (setq
-    doom-modeline-project-detection 'project
-    doom-modeline-project-detection 'ffip
+    doom-modeline-buffer-file-name-style 'relative-to-project
+    doom-modeline-column-zero-based nil
+    doom-modeline-buffer-encoding nil
+    doom-modeline-minor-modes t
+    doom-modeline-project-detection 'projectile
     doom-modeline-enable-word-count 10
     doom-modeline-icon (display-graphic-p)
     doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode)
-    doom-modeline-buffer-file-name-style 'relative-to-project doom-modeline-modal-icon nil
     )
   :hook (after-init . doom-modeline-mode)
   )
