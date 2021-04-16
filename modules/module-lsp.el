e7accd8232be8c5009f7da1fa05e0ac03e62e9bd
@slovacus <slovacus@gmail.com>
2021-04-15 20:53:03 -0500

    refactor: configuration modules for lsp (#324)

--- modules/module-lsp.el
+++ modules/module-lsp.el
@@ -1,12 +1,6 @@
 ;;; module-lsp.el --- lsp config.
 ;;; code:
 
-;; Optional - provides snippet support.
-(use-package yasnippet
-  :ensure t
-  :commands yas-minor-mode
-  :hook (go-mode . yas-minor-mode))
-
 ;; optionally if you want to use debugger
 (use-package dap-mode)
 ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
@@ -19,24 +13,37 @@
 (use-package lsp-mode
 	:ensure nil
   :commands (lsp lsp-deferred)
-  :config (progn
-	          (setq gc-cons-threshold 100000000)
-	          (setq read-process-output-max (* 2 1024 1024)) ;; 1mb
-	          (setq company-minimum-prefix-length 1
-		          company-idle-delay 0.0) ;; default is 0.2
-            (setq lsp-prefer-flymake nil))
-  :hook ((lsp-mode . lsp-enable-which-key-integration)
-          (python-mode . lsp-deferred)
+  :config
+  (progn
+    (setq lsp-auto-configure t)
+    (setq lsp-auto-guess-root t )
+	  (setq gc-cons-threshold 100000000)
+	  (setq read-process-output-max (* 2 1024 1024)) ;; 1mb
+	  (setq company-minimum-prefix-length 1
+		  company-idle-delay 0.0) ;; default is 0.2
+    (setq lsp-prefer-flymake nil)
+    ;; don't set flymake or lsp-ui so the default linter doesn't get trampled
+    (setq lsp-diagnostic-package :none)
+    )
+  (evil-leader/set-key
+    "jd"  #'lsp-goto-type-definition ; (j)ump to (d)efinition
+    "jb"  #'xref-pop-marker-stack)  ; (j)ump (b)ack to marker
+  :hook (
+          (lsp-mode . lsp-enable-which-key-integration)
+          (python-mode . lsp-deferred) ;; https://github.com/palantir/python-language-server
+          (sh-mode . lsp-deferred) ;; npm i -g bash-language-server
           (lua-mode . lsp-deferred)
           (c-mode . lsp-deferred)
           (go-mode . lsp-deferred)
           (ruby-mode . lsp-deferred)
           (java-mode . lsp-deferred)
-          (js-mode . lsp-deferred)
-          (web-mode . lsp-deferred)
+          (json-mode . lsp-deferred) ;; yarn global add vscode-json-languageserver
+          ((js2-mode rjsx-mode typescript-mode) . lsp-deferred) ;; https://www.chadstovern.com/javascript-in-emacs-revisited/
+          (web-mode . lsp-deferred) ;; yarn global add hmtl-ls
           (terraform-mode	. lsp-deferred)
           (rust-mode . lsp-deferred)
-          (html-mode . lsp-deferred))
+          (html-mode . lsp-deferred)
+          )
   :custom
   (
     setq lsp-enable-indentation nil
@@ -47,19 +54,22 @@
 		lsp-eldoc-render-all t
 		lsp-enable-file-watchers nil
 		lsp-highlight-symbol-at-point nil
-    ))
+    )
+  )
 
 (use-package lsp-ui
-	:after lsp-mode
-	:ensure t
-  :commands lsp-ui-mode
-	:config
-	(setq lsp-ui-sideline-enable nil
-		lsp-ui-doc-enable nil
-		lsp-ui-flycheck-enable t
-		lsp-prefer-flymake nil
-		lsp-ui-imenu-enable t
-		lsp-ui-sideline-ignore-duplicate t))
+  :defer t
+  :config
+  (setq lsp-ui-sideline-enable t
+    ;; disable flycheck setup so default linter isn't trampled
+    lsp-ui-flycheck-enable nil
+    lsp-ui-sideline-show-symbol nil
+    lsp-ui-sideline-show-hover nil
+    lsp-ui-sideline-show-code-actions nil
+    lsp-ui-peek-enable nil
+    lsp-ui-imenu-enable nil
+    lsp-ui-doc-enable nil)
+  )
 
 (use-package helm-lsp :ensure t :commands helm-lsp-workspace-symbol)
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
