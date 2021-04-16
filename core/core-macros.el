032ee3a1341d6a1efc7bbf1eefdb86e3574bb06f
@slovacus <slovacus@gmail.com>
2021-04-15 20:53:37 -0500

    chore: implement code macros (#324)

---
+++ core/core-macros.el
@@ -0,0 +1,29 @@
+;;; core-macros.el --- Macro definitions. -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'core-vars)
+(require 'hierarchy)
+(require 'seq)
+
+;;; Convenience functions for leader bindings
+
+(declare-function 'general-define-key "general")
+
+(defmacro core-leader (&rest args)
+  "Bind ARGS as leader bindings."
+  (declare (indent 0))
+  `(progn
+     (require 'general)
+     ,@(cl-loop for (key func doc) in args
+                collect
+                `(progn
+                   (when ,doc
+                     (which-key-add-key-based-replacements ,(concat core-leader-key " " key) ,doc))
+                   (general-define-key :prefix core-leader-key :states '(normal motion) :keymaps 'override ,key ,func)))))
+
+(provide 'core-macros)
+
+;;; core-macros.el ends here
