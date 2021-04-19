a7de1354b46ea8195cd96cc127c8086cef23c935
@slovacus <slovacus@gmail.com>
2021-04-19 11:54:33 -0500

    refactor: name functions core (#330)

--- core/core-macros.el
+++ core/core-macros.el
@@ -16,11 +16,28 @@
   `(progn
      (require 'general)
      ,@(cl-loop for (key func doc) in args
-                collect
-                `(progn
-                   (when ,doc
-                     (which-key-add-key-based-replacements ,(concat core-leader-key " " key) ,doc))
-                   (general-define-key :prefix core-leader-key :states '(normal motion) :keymaps 'override ,key ,func)))))
+         collect
+         `(progn
+            (when ,doc
+              (which-key-add-key-based-replacements ,(concat core-leader-key " " key) ,doc))
+            (general-define-key :prefix core-leader-key :states '(normal motion) :keymaps 'override ,key ,func)))))
+
+(defmacro def (name &rest body)
+  (declare (indent 1) (debug t))
+  `(defun ,name (&optional _arg)
+     ,(if (stringp (car body)) (car body))
+     (interactive "p")
+     ,@(if (stringp (car body)) (cdr `,body) body)))
+
+(defmacro λ (&rest body)
+  (declare (indent 1) (debug t))
+  `(lambda ()
+     (interactive)
+     ,@body))
+
+(defmacro add-λ (hook &rest body)
+  (declare (indent 1) (debug t))
+  `(add-hook ,hook (lambda () ,@body)))
 
 (provide 'core-macros)
 
