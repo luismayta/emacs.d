b61fa305b3ab53aa2d510c7ff617256d4d6c962b
@slovacus <slovacus@gmail.com>
2021-04-19 11:55:29 -0500

    feat: implement command args for fd (#330)

--- modules/module-projects.el
+++ modules/module-projects.el
@@ -2,6 +2,7 @@
 ;;; code:
 
 (require 'core-vars)
+(require 'core-functions)
 
 (use-package projectile
   :requires (ivy)
@@ -9,28 +10,41 @@
   ("C-c q" . projectile-command-map)
   :hook (after-init . projectile-mode)
   :init
-  (setq projectile-keymap-prefix (kbd "C-c q"))
-  (setq projectile-mode-line-prefix ""
-    projectile-sort-order 'recentf
-    )
+  (setq projectile-keymap-prefix (kbd "C-c q")
+    projectile-mode-line-prefix ""
+    projectile-sort-order 'recentf)
   :config
-  (setq projectile-completion-system 'ivy)
-  (setq projectile-indexing-method 'alien)
+  (setq projectile-completion-system 'ivy
+    projectile-sort-order 'recentf
+    projectile-indexing-method 'alien
+    projectile-enable-caching nil)
   (when IS-WINDOWS
     (setq projectile-git-submodule-command nil
-      projectile-enable-caching nil)))
+      ))
+  ;; fd is super fast. Use it if available.
+  (when (executable-find "fd")
+    (setq projectile-hg-command fd-command)
+    (setq projectile-git-command fd-command)
+    (setq projectile-fossil-command fd-command)
+    (setq projectile-bzr-command fd-command)
+    (setq projectile-darcs-command fd-command)
+    (setq projectile-svn-command fd-command)
+    (setq projectile-generic-command fd-command))
+  (projectile-mode))
 
 (use-package counsel-projectile
   :after projectile
   :requires (evil-leader)
   :bind-keymap
   ("C-c q" . counsel-projectile-command-map)
+  :hook (counsel-mode . counsel-projectile-mode)
   :config
   (counsel-projectile-mode)
   :init
-  (evil-leader/set-key "fe" 'find-file)
   (evil-leader/set-key "ff" 'counsel-projectile-ag)
-  (evil-leader/set-key "fp" 'counsel-projectile-find-file)
+  (evil-leader/set-key "fp" 'core/smart-find-file)
+  (evil-leader/set-key "fwp" 'counsel-projectile-switch-project)
+  (setq counsel-projectile-grep-initial-input '(ivy-thing-at-point))
   )
 
 (use-package helm-ghq
