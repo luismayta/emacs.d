366dfd479d51fadaff489848f450a65ad248eed1
luis mayta <slovacus@gmail.com>
2021-12-26 22:00:08 -0500

    chore: implement packages solidity (#363)

--- modules/module-ethereum.el
+++ modules/module-ethereum.el
@@ -1,8 +1,23 @@
-;;; module-ethereum.el --- Ethereum settings.
+;;; module-ethereum.el --- Configure solidity-mode -*- lexical-binding: t -*-
 ;;; code:
 
-;; (use-package solidity-mode)
-;; (use-package company-solidity)
+(use-package solidity-mode
+  :ensure-system-package
+  (
+    (solcjs . "yarn global add solc")
+    (solium . "yarn global add ethlint")
+    )
+  )
+
+(use-package solidity-flycheck
+  :straight t
+  :defer 20
+  :ensure t)
+
+(use-package company-solidity
+  :straight t
+  :defer 20
+  :ensure t)
 
 (provide 'module-ethereum)
 ;;; module-ethereum.el ends here
